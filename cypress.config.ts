a1a0239669d747e5e8c07cef22d71889bf5a69d0
TomCusack-Softwire <Tom.Cusack@softwire.com>
2023-07-11 18:00:32 +0100

    Reformatting random files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,30 +1,31 @@
-import { defineConfig } from "cypress";
 import registerCodeCoverageTasks from "@cypress/code-coverage/task";
+import { defineConfig } from "cypress";
 
 import * as dotenv from "dotenv";
-dotenv.config({path: "./.env.local"});
+
+dotenv.config({ path: "./.env.local" });
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      registerCodeCoverageTasks(on, config);
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+            registerCodeCoverageTasks(on, config);
 
-      config.env = {
-        "TEST_USER": process.env.NEXT_PUBLIC_CYPRESS_TEST_USER!,
-        "TEST_PASS": process.env.NEXT_PUBLIC_CYPRESS_TEST_PASS!,
-      }
+            config.env = {
+                TEST_USER: process.env.NEXT_PUBLIC_CYPRESS_TEST_USER!,
+                TEST_PASS: process.env.NEXT_PUBLIC_CYPRESS_TEST_PASS!,
+            };
 
-      return config;
+            return config;
+        },
+        baseUrl: "http://localhost:3000",
     },
-    baseUrl: "http://localhost:3000",
-  },
 
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
+    component: {
+        devServer: {
+            framework: "next",
+            bundler: "webpack",
+        },
+        specPattern: "src/**/*.cy.{ts,tsx}",
     },
-    "specPattern": "src/**/*.cy.{ts,tsx}",
-  },
 });
