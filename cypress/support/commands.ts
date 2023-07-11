97c01f7dcefaf82e5c8f055eb2f53eb08e98b518
TomCusack-Softwire <Tom.Cusack@softwire.com>
2023-07-11 17:56:58 +0100

    Add login caching to cypress tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -14,17 +14,19 @@ Cypress.Commands.add("login", () => {
     const email = Cypress.env("TEST_USER");
     const password = Cypress.env("TEST_PASS");
 
-    cy.intercept("http://localhost:3000/auth/callback").as("auth");
+    cy.session(email, () => {
+        cy.intercept("http://localhost:3000/auth/callback").as("auth");
 
-    cy.visit("/");
-    cy.url().should("include", "/login");
+        cy.visit("/");
+        cy.url().should("include", "/login");
 
-    cy.get("input#email").type(email);
-    cy.get("input#password").type(password);
-    cy.get("button[type=submit]").as("login_button").click();
-    cy.get("@login_button").should("be.enabled");
+        cy.get("input#email").type(email);
+        cy.get("input#password").type(password);
+        cy.get("button[type=submit]").as("login_button").click();
+        cy.get("@login_button").should("be.enabled");
+    });
 
-    cy.visit("/clients"); // should redirect automatically but is flaky
+    cy.visit("/clients"); // if session cache is used, then it doesn't redirect - so redirect manually!
 
     cy.url().should("include", "/clients");
 });
