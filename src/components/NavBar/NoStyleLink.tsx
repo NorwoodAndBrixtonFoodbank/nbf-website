dcd11953293ddf835da9fd86fb7e4078f2e934c9
daniel-conlan-softwire <Daniel.Conlan@softwire.com>
2023-07-14 15:57:13 +0100

    ﻿Restyled and refactored navigation bar implementation.

---
+++ src/components/NavBar/NoStyleLink.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const NoStyleLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+`;
+
+export default NoStyleLink;
