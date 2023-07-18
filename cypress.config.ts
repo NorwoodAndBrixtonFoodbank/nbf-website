be2c563cbb6c338d2d52af13559da83f4f742428
Tom Cusack <104017547+TomCusack-Softwire@users.noreply.github.com>
2023-07-18 17:02:14 +0100

    Set the Cypress port properly (#17)

--- cypress.config.ts
+++ cypress.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 
             return config;
         },
-        baseUrl: `http://localhost:3${process.env.MANUAL_TEST ? 1 : 2}00`,
+        baseUrl: "http://localhost:3200",
     },
 
     component: {
