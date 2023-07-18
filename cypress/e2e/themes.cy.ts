7d13e0546525ac25d319fb1e764d6d1c9aaf7609
TomCusack-Softwire <Tom.Cusack@softwire.com>
2023-07-18 16:04:14 +0100

    dodgy patch to see if it works

--- cypress/e2e/themes.cy.ts
+++ cypress/e2e/themes.cy.ts
@@ -1,16 +1,14 @@
 import { hexToRgb } from "@mui/material";
 
 describe("Light and dark mode buttons work", () => {
-    beforeEach(() => {
-        cy.login();
-    });
-
     it("Light mode button works", () => {
+        cy.login();
         cy.get(".light-button").click();
         cy.get("body").should("have.css", "background-color", hexToRgb("#eeeeee"));
     });
 
     it("dark mode button works", () => {
+        cy.login();
         cy.get(".dark-button").click();
         cy.get("body").should("have.css", "background-color", hexToRgb("#1a1a1a"));
     });
