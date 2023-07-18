4028a010f9ae83c7ae05a8fc820d83398b5ddb97
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-07-18 15:47:06 +0100

    Started calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,10 +1,31 @@
+import supabase, { Schema } from "@/supabase";
 import { Metadata } from "next";
 import React from "react";
 
-const CalendarPage: React.FC<{}> = () => {
+interface parcelRow {
+    collection_centre: string | null;
+    collection_datetime: Date | null;
+    family_id: string;
+    packing_datetime: Date | null;
+    primary_key: string;
+}
+
+const dataFetch: () => Promise<Schema["parcels"][]> = async () => {
+    const response = await supabase.from("parcels").select();
+    return response.data ?? [];
+};
+
+const CalendarPage: () => Promise<React.ReactElement> = async () => {
+    const data: Schema["parcels"][] = await dataFetch();
+
     return (
         <main>
-            <h1>Calendar Page</h1>
+            <h1> Clients Page </h1>
+
+            <p> Testing Supabase fetching </p>
+
+            {/* This should be a separate component which is passed data via props */}
+            <pre>{JSON.stringify(data, null, 4)}</pre>
         </main>
     );
 };
