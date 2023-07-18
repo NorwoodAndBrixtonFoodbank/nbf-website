92966dfa5fa994f9158acb65404d3bfcc50cb6b8
SamYap-Softwire <samyun.yap@softwire.com>
2023-07-18 09:22:55 +0100

    VFB-16 Add test for light and dark mode buttons

---
+++ cypress/e2e/themes.cy.ts
@@ -0,0 +1,17 @@
+import { hexToRgb } from "@mui/material";
+
+describe("Light and dark mode buttons work", () => {
+    beforeEach(() => {
+        cy.login();
+    });
+
+    it("Light mode button works", () => {
+        cy.get(".light-button").click();
+        cy.get("body").should("have.css", "background-color", hexToRgb("#eeeeee"));
+    });
+
+    it("dark mode button works", () => {
+        cy.get(".dark-button").click();
+        cy.get("body").should("have.css", "background-color", hexToRgb("#1a1a1a"));
+    });
+});
