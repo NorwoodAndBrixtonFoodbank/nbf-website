e713ab693df1f9919af34f3a5039adf0bdb0a2be
Varun Latthe <varun.latthe@gmail.com>
2023-07-20 15:22:24 +0100

    VFB-6: Add column selection

---
+++ src/components/Tables/Accordion.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Accordion, Checkbox, AccordionSummary, AccordionDetails } from "@mui/material";
+import { styled } from "styled-components";
+
+type FilterAccordionProps = {
+    toggleableHeaders?: string[];
+    shownHeaderKeys: string[];
+    setShownHeaderKeys: (headers: string[]) => void;
+    headers: [string, string][];
+};
+
+const Styling = styled.div`
+    flex-grow: 1;
+    height: 3rem;
+    overflow: visible;
+    z-index: 2;
+    & .MuiPaper-root {
+        background-color: ${(props) => props.theme.surfaceBackgroundColor};
+        & .MuiAccordionDetails-root {
+            display: flex;
+            flex-wrap: wrap;
+            flex-basis: auto;
+            gap: 1rem;
+        }
+    }
+`;
+
+const ContainerDiv = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+`;
+const FilterAccordion: React.FC<FilterAccordionProps> = ({
+    toggleableHeaders,
+    shownHeaderKeys,
+    setShownHeaderKeys,
+    headers,
+}) => {
+    return (
+        <Styling>
+            <Accordion>
+                <AccordionSummary>
+                    <p>Select Columns</p>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {(toggleableHeaders ?? []).map((key) => {
+                        return (
+                            <ContainerDiv>
+                                <Checkbox
+                                    key={key}
+                                    checked={shownHeaderKeys.includes(key)}
+                                    onChange={(event) => {
+                                        if (event.target.checked) {
+                                            setShownHeaderKeys([...shownHeaderKeys, key]);
+                                        } else {
+                                            setShownHeaderKeys(
+                                                shownHeaderKeys.filter(
+                                                    (shownKey) => shownKey !== key
+                                                )
+                                            );
+                                        }
+                                    }}
+                                />
+                                <p>
+                                    {headers.find(
+                                        ([headerKey, _value]) => headerKey === key
+                                    )?.[1] ?? key}
+                                </p>
+                            </ContainerDiv>
+                        );
+                    })}
+                </AccordionDetails>
+            </Accordion>
+        </Styling>
+    );
+};
+
+export default FilterAccordion;
--- src/components/Tables/TableFilterBar.tsx
+++ src/components/Tables/TableFilterBar.tsx
@@ -1,33 +1,50 @@
 "use client";
 
 import React from "react";
+import { styled } from "styled-components";
+import FilterAccordion from "./Accordion";
 
 export interface FilterText {
     [key: string]: string;
 }
+const StyledFilterAccordion = styled(FilterAccordion)`
+    width: 100%;
+`;
 
 interface Props {
     filterText: FilterText;
     onFilter: (event: React.ChangeEvent<HTMLInputElement>, filterField: string) => void;
     handleClear: () => void;
-    headers: FilterText;
+    headers: [string, string][];
+    filterKeys: string[];
+    toggleableHeaders?: string[];
+    setShownHeaderKeys: (headers: string[]) => void;
+    shownHeaderKeys: string[];
 }
 
 const TableFilterBar: React.FC<Props> = (props) => {
     return (
         <>
-            {Object.entries(props.headers).map(([key, value]) => {
-                return (
-                    <input
-                        key={key}
-                        type="text"
-                        value={props.filterText[key] ?? ""}
-                        placeholder={`Filter by ${value}`}
-                        onChange={(event) => props.onFilter(event, key)}
-                    />
-                );
-            })}
             <button onClick={props.handleClear}>Clear</button>
+            {props.headers
+                .filter(([key, _value]) => props.filterKeys?.includes(key) ?? true)
+                .map(([key, value]) => {
+                    return (
+                        <input
+                            key={key}
+                            type="text"
+                            value={props.filterText[key] ?? ""}
+                            placeholder={`Filter by ${value}`}
+                            onChange={(event) => props.onFilter(event, key)}
+                        />
+                    );
+                })}
+            <StyledFilterAccordion
+                toggleableHeaders={props.toggleableHeaders}
+                shownHeaderKeys={props.shownHeaderKeys}
+                setShownHeaderKeys={props.setShownHeaderKeys}
+                headers={props.headers}
+            />
         </>
     );
 };
