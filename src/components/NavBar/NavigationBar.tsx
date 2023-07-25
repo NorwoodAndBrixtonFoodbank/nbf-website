4f4bf137c891a2727a7de7d5a721a15f39a88d74
Varun Latthe <varun.latthe@gmail.com>
2023-07-25 14:47:56 +0100

    VFB-13: rewrote it

--- src/components/NavBar/NavigationBar.tsx
+++ src/components/NavBar/NavigationBar.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Link from "next/link";
+import { styled } from "styled-components";
 import Button from "@mui/material/Button";
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import ProfilePicture from "@/components/NavBar/ProfilePicture";
-import MobileNavigationMenu from "@/components/NavBar/MobileNavigationMenu";
-import NoStyleLink from "@/components/NavBar/NoStyleLink";
-import SignOutButton from "@/components/NavBar/SignOutButton";
+import { useRouter } from "next/navigation";
+
+export const PageButton = styled(Button)`
+    color: white;
+    font-size: 1.25rem;
+    margin: 10px;
+    &:hover {
+        background-color: ${(props) => props.theme.primaryBackgroundColor};
+    }
+`;
+
+const UnstyledLink = styled(Link)`
+    text-decoration: none;
+    display: contents;
+`;
+
+const LogoLink = styled(UnstyledLink)`
+    display: contents;
+`;
 
 const Logo = styled.img`
     max-width: 100%;
@@ -23,129 +39,168 @@ const LogoDiv = styled.div`
     background-color: white;
     border-radius: 10px;
     overflow: hidden;
-    padding: 2px 2px;
-    box-shadow: 2px 2px 5px #60ae2d;
-    width: 115px;
-    height: 50px;
+    padding: 2px;
+    box-shadow: 2px 2px 5px ${(props) => props.theme.primaryBackgroundColor};
     display: flex;
     justify-content: center;
-    margin: 10px;
+    flex-basis: auto;
+    margin: 10px 0;
+    height: 80%;
+    object-fit: cover;
 `;
 
-const DesktopDiv = styled.div`
+const AppBarInner = styled.div`
+    display: flex;
+    height: 4rem;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.5rem;
+`;
+
+const SmSideElement = styled.div`
+    display: flex;
+    align-items: center;
+    width: 8rem;
+
+    @media (min-width: 800px) {
+        display: none;
+    }
+`;
+
+const LgNavElement = styled.div`
     display: none;
+    flex-grow: 1;
+    justify-content: end;
+    gap: 1rem;
 
     @media (min-width: 800px) {
         display: flex;
-        flex-direction: row;
-        flex-grow: 1;
-        justify-content: space-between;
-        align-content: center;
-        align-items: center;
     }
 `;
 
-const MobileDiv = styled.div`
-    display: none;
-
-    @media (max-width: 799px) {
-        display: flex;
-        flex-direction: row;
-        flex-grow: 1;
-        justify-content: space-between;
-        align-content: center;
-        align-items: center;
-    }
+const Spacer = styled.div`
+    flex-grow: 1;
 `;
 
-const StyledAppBar = styled(AppBar)`
-    background-color: green;
-    position: sticky;
-    padding: 5px;
-`;
-
-// For use with <ProfilePicture /> upon implementation
-//
-// const ProfilePictureDiv = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-// `;
-
-export const PageButton = styled(Button)`
-    color: white;
-    font-size: 1.25rem;
-    margin: 10px;
-    &:hover {
-        background-color: #60ae2d;
-    }
-`;
-
-const PageButtonsDiv = styled.div`
+const DrawerInner = styled.div`
     display: flex;
-    align-items: baseline;
+    flex-direction: column;
+    justify-content: start;
+    align-items: stretch;
+    padding: 1rem;
+    width: 15rem;
 `;
 
-const NavigationBar: React.FC<{}> = () => {
-    const pages: [string, string][] = [
+const LogoNavElement: React.FC<{}> = () => (
+    <LogoLink href="/">
+        <LogoDiv>
+            <Logo alt="Vauxhall Foodbank Logo" src="/logo.png" />
+        </LogoDiv>
+    </LogoLink>
+);
+
+const SignOutButton: React.FC<{}> = () => {
+    const router = useRouter();
+
+    const handleSignOutClick = (): void => {
+        fetch("/auth/signout", {
+            method: "POST",
+        }).then(() => {
+            router.push("/login");
+        });
+    };
+
+    return (
+        <IconButton
+            size="medium"
+            aria-label="Sign Out Button"
+            id="nav-sign-out"
+            onClick={handleSignOutClick}
+        >
+            <LogoutIcon />
+        </IconButton>
+    );
+};
+
+const Gap = styled.div`
+    width: 1rem;
+`;
+
+const DrawerButtonWrapper = styled.div`
+    padding: 1rem;
+    border-bottom: 1px solid ${(props) => props.theme.primaryBackgroundColor};
+
+    &:last-child {
+        border-bottom: none;
+    }
+`;
+
+const DrawerButton = styled(Button)`
+    width: 100%;
+`;
+
+const ResponsiveAppBar: React.FC<{}> = () => {
+    const [drawer, setDrawer] = React.useState(false);
+
+    const openDrawer = (): void => {
+        setDrawer(true);
+    };
+
+    const closeDrawer = (): void => {
+        setDrawer(false);
+    };
+
+    const pages = [
         ["Clients", "/clients"],
         ["Lists", "/lists"],
         ["Calendar", "/calendar"],
     ];
 
-    const [mobileNavPosition, setMobileNavPosition] = useState<HTMLElement | null>(null);
-
-    const handleOpenMobileNavMenu = (event: React.MouseEvent<HTMLElement>): void => {
-        setMobileNavPosition(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = (): void => {
-        setMobileNavPosition(null);
-    };
-
     return (
-        <StyledAppBar>
-            <MobileDiv>
-                <Box color="white">
-                    <IconButton
-                        size="large"
-                        aria-label="Mobile Menu Button"
-                        onClick={handleOpenMobileNavMenu}
-                        color="inherit"
-                    >
-                        <FontAwesomeIcon icon={faBars} />
-                    </IconButton>
-                    <MobileNavigationMenu
-                        pages={pages}
-                        mobileNavPosition={mobileNavPosition}
-                        setMobileNavPosition={setMobileNavPosition}
-                    />
-                </Box>
-
-                <LogoDiv>
-                    <Logo alt="Vauxhall Foodbank Logo" src="/logo.png" />
-                </LogoDiv>
-
-                <SignOutButton />
-            </MobileDiv>
-
-            <DesktopDiv>
-                <LogoDiv>
-                    <Logo alt="Vauxhall Foodbank Logo" src="/logo.png" />
-                </LogoDiv>
-
-                <PageButtonsDiv>
+        <>
+            <SwipeableDrawer open={drawer} onClose={closeDrawer} onOpen={openDrawer}>
+                <DrawerInner>
                     {pages.map(([page, link]) => (
-                        <NoStyleLink href={link} key={page}>
-                            <PageButton onClick={handleCloseNavMenu}>{page}</PageButton>
-                        </NoStyleLink>
+                        <DrawerButtonWrapper key={page}>
+                            <UnstyledLink href={link} onClick={closeDrawer}>
+                                <DrawerButton variant="text">{page}</DrawerButton>
+                            </UnstyledLink>
+                        </DrawerButtonWrapper>
                     ))}
-                </PageButtonsDiv>
-
-                <SignOutButton />
-            </DesktopDiv>
-        </StyledAppBar>
+                </DrawerInner>
+            </SwipeableDrawer>
+            <AppBar position="static">
+                <AppBarInner>
+                    <SmSideElement>
+                        <IconButton
+                            size="medium"
+                            aria-label="Mobile Menu Button"
+                            onClick={openDrawer}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Spacer />
+                    </SmSideElement>
+                    <LogoNavElement />
+                    {/* TODO: VFB-16 need to fix nav colours */}
+                    <LgNavElement>
+                        {pages.map(([page, link]) => (
+                            <UnstyledLink key={page} href={link} onClick={closeDrawer}>
+                                <Button variant="outlined" color="secondary">
+                                    {page}
+                                </Button>
+                            </UnstyledLink>
+                        ))}
+                        <Gap />
+                        <SignOutButton />
+                    </LgNavElement>
+                    <SmSideElement>
+                        <Spacer />
+                        <SignOutButton />
+                    </SmSideElement>
+                </AppBarInner>
+            </AppBar>
+        </>
     );
 };
-
-export default NavigationBar;
+export default ResponsiveAppBar;
--- src/components/NavBar/NoStyleLink.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import Link from "next/link";
-
-const NoStyleLink = styled(Link)`
-    text-decoration: none;
-    color: inherit;
-`;
-
-export default NoStyleLink;
