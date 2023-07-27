5daa21ee30e1ab5134215a8963ed73b2670f3f1f
TomCusack-Softwire <Tom.Cusack@softwire.com>
2023-07-27 11:51:18 +0100

    Include login page and separate snapshot-only tests

--- cypress/e2e/snapshot.cy.ts
+++ cypress/e2e/snapshot.cy.ts
@@ -2,10 +2,16 @@ import PAGES from "./PAGES";
 
 describe("Screenshot tests", () => {
     PAGES.map((page) => {
-        it(page.friendly_name, () => {
+        it(`${page.friendly_name} page`, () => {
            cy.login();
            cy.visit(page.url);
-           cy.compareSnapshot(page.friendly_name);
+           cy.compareSnapshot(page.friendly_name, 0.05);
         });
+    });
+
+    it("Login page", () => {
+        cy.visit("/login");
+        cy.url().should("include", "/login");
+        cy.compareSnapshot("Login", 0.05);
     })
 });
