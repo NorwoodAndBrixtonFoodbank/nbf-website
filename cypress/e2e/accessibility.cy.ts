e80e897645ae2cedd7f094b932d3b7b79e47398d
Dan Wilkins <Dan.Wilkins@software.com>
2023-07-31 16:26:47 +0100

    fixed accessibility

--- cypress/e2e/accessibility.cy.ts
+++ cypress/e2e/accessibility.cy.ts
@@ -24,7 +24,7 @@ describe("Accessibility tests in light mode", () => {
 
     it("Checks login page", () => {
         cy.visit("/clients");
-        cy.get("button[aria-label='Sign Out Button']").filter(":visible").click();
+        cy.get("button[aria-label='Sign Out Button']").click();
         cy.url().should("include", "/login");
 
         cy.checkAccessibility();
@@ -38,14 +38,14 @@ describe("Accessibility tests in dark mode", () => {
 
     it("Checks clients page", () => {
         cy.visit("/clients");
-        cy.get("label[aria-label='Theme Switch']").filter(":visible").click();
+        cy.get("label[aria-label='Theme Switch']").click();
 
         cy.checkAccessibility();
     });
 
     it("Checks lists page", () => {
         cy.visit("/lists");
-        cy.get("label[aria-label='Theme Switch']").filter(":visible").click();
+        cy.get("label[aria-label='Theme Switch']").click();
 
         cy.checkAccessibility();
     });
@@ -53,15 +53,15 @@ describe("Accessibility tests in dark mode", () => {
     it("Checks calendar page", () => {
         cy.visit("/calendar");
         cy.get("table").should("be.visible");
-        cy.get("label[aria-label='Theme Switch']").filter(":visible").click();
+        cy.get("label[aria-label='Theme Switch']").click();
 
         cy.checkAccessibility();
     });
 
     it("Checks login page", () => {
         cy.visit("/clients");
-        cy.get("label[aria-label='Theme Switch']").filter(":visible").click();
-        cy.get("button[aria-label='Sign Out Button']").filter(":visible").click();
+        cy.get("label[aria-label='Theme Switch']").click();
+        cy.get("button[aria-label='Sign Out Button']").click();
         cy.url().should("include", "/login");
 
         cy.checkAccessibility();
