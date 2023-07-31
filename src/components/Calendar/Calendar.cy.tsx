208fd8bec0b3a62c6b486789963622cf400bd125
Varun Latthe <66573927+Var1377@users.noreply.github.com>
2023-07-31 12:02:49 +0100

    Fix calendar tests (#29)

--- src/components/Calendar/Calendar.cy.tsx
+++ src/components/Calendar/Calendar.cy.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import Calendar, { CalendarEvent } from "@/components/Calendar/Calendar";
 
 describe("<Calendar />", () => {
-    const today = new Date();
-    const tomorrow = new Date();
-    tomorrow.setDate(today.getDate() + 1);
+    const testDate = new Date("2021-04-05");
+    const dayAfterTestDate = new Date("2021-04-06");
+    dayAfterTestDate.setDate(testDate.getDate() + 1);
 
     const sampleEvents: CalendarEvent[] = [
         {
             id: "a",
             title: "event1",
-            start: today,
-            end: tomorrow,
+            start: testDate,
+            end: dayAfterTestDate,
             allDay: true,
         },
         {
             id: "b",
             title: "event2",
-            start: today,
-            end: today,
+            start: testDate,
+            end: testDate,
             allDay: false,
             description: "a piece of description text",
         },
@@ -29,49 +29,49 @@ describe("<Calendar />", () => {
     });
 
     it("calendar is set to the current month when rendered in dayGridMonth", () => {
-        const currentMonthYear = today.toLocaleDateString("en-GB", {
+        const currentMonthYear = testDate.toLocaleDateString("en-GB", {
             month: "long",
             year: "numeric",
         });
 
-        cy.mount(<Calendar initialEvents={[]} />);
+        cy.mount(<Calendar initialEvents={[]} initialDate={testDate} />);
         cy.get(".fc-toolbar-title").should("have.text", currentMonthYear);
     });
 
     it("events render", () => {
-        cy.mount(<Calendar initialEvents={sampleEvents} />);
+        cy.mount(<Calendar initialEvents={sampleEvents} initialDate={testDate} />);
         cy.get(".fc-event").should("be.visible");
     });
 
     it("can change view to timeGridDay", () => {
-        cy.mount(<Calendar initialEvents={[]} />);
+        cy.mount(<Calendar initialEvents={[]} initialDate={testDate} />);
         cy.get("button.fc-timeGridDay-button").click();
         cy.get(".fc-timeGridDay-view").should("be.visible");
     });
 
     it("can change view to timeGridWeek", () => {
-        cy.mount(<Calendar initialEvents={[]} />);
+        cy.mount(<Calendar initialEvents={[]} initialDate={testDate} />);
         cy.get("button.fc-timeGridWeek-button").click();
         cy.get(".fc-timeGridWeek-view").should("be.visible");
     });
 
     it("can change view between months in dayGridMonth", () => {
-        const prevMonth: Date = new Date();
-        prevMonth.setMonth((today.getMonth() + 11) % 12);
+        const prevMonth = new Date(testDate);
+        prevMonth.setMonth((testDate.getMonth() + 11) % 12);
         const prevMonthYear = prevMonth.toLocaleString("en-GB", { month: "long", year: "numeric" });
 
-        cy.mount(<Calendar initialEvents={[]} />);
+        cy.mount(<Calendar initialEvents={[]} initialDate={testDate} />);
         cy.get("button.fc-prev-button").click();
 
         cy.get(".fc-toolbar-title").should("have.text", prevMonthYear);
     });
 
     it("can change view between days in timeGridDay", () => {
-        const tomorrowDMY = tomorrow
+        const tomorrowDMY = dayAfterTestDate
             .toLocaleString("en-GB", { day: "numeric", month: "long", year: "numeric" })
             .split(" ");
 
-        cy.mount(<Calendar initialEvents={[]} view="timeGridDay" />);
+        cy.mount(<Calendar initialEvents={[]} view="timeGridDay" initialDate={testDate} />);
         cy.get("button.fc-next-button").click();
 
         cy.get(".fc-toolbar-title").should(
@@ -81,23 +81,23 @@ describe("<Calendar />", () => {
     });
 
     it("shows description when event with description is clicked", () => {
-        cy.mount(<Calendar initialEvents={sampleEvents} />);
+        cy.mount(<Calendar initialEvents={sampleEvents} initialDate={testDate} />);
         cy.get(".fc-event-title").contains("event2").parent().click();
         cy.get(".MuiDialog-container").should("include.text", "a piece of description text");
     });
 
     it("does not show description when event without description is clicked", () => {
-        cy.mount(<Calendar initialEvents={sampleEvents} />);
+        cy.mount(<Calendar initialEvents={sampleEvents} initialDate={testDate} />);
         cy.get(".fc-event-title").contains("event1").click();
         cy.get(".MuiDialog-container").should("not.include.text", "description");
     });
 
     it("shows correct date for full day event", () => {
-        cy.mount(<Calendar initialEvents={sampleEvents} />);
+        cy.mount(<Calendar initialEvents={sampleEvents} initialDate={testDate} />);
         cy.get(".fc-event-title").contains("event1").click();
         cy.get(".MuiDialog-container").should(
             "include.text",
-            today.toLocaleDateString("en-GB", {
+            testDate.toLocaleDateString("en-GB", {
                 weekday: "long",
                 year: "numeric",
                 month: "long",
@@ -106,7 +106,7 @@ describe("<Calendar />", () => {
         );
         cy.get(".MuiDialog-container").should(
             "include.text",
-            tomorrow.toLocaleDateString("en-GB", {
+            dayAfterTestDate.toLocaleDateString("en-GB", {
                 weekday: "long",
                 year: "numeric",
                 month: "long",
@@ -115,14 +115,14 @@ describe("<Calendar />", () => {
         );
         cy.get(".MuiDialog-container").should(
             "not.include.text",
-            today.toLocaleTimeString("en-GB", {
+            testDate.toLocaleTimeString("en-GB", {
                 hour: "numeric",
                 minute: "numeric",
             })
         );
         cy.get(".MuiDialog-container").should(
             "not.include.text",
-            tomorrow.toLocaleTimeString("en-GB", {
+            dayAfterTestDate.toLocaleTimeString("en-GB", {
                 hour: "numeric",
                 minute: "numeric",
             })
@@ -130,11 +130,11 @@ describe("<Calendar />", () => {
     });
 
     it("shows correct date for non-full day event", () => {
-        cy.mount(<Calendar initialEvents={sampleEvents} />);
+        cy.mount(<Calendar initialEvents={sampleEvents} initialDate={testDate} />);
         cy.get(".fc-event-title").contains("event2").parent().click();
         cy.get(".MuiDialog-container").should(
             "include.text",
-            today.toLocaleDateString("en-GB", {
+            testDate.toLocaleDateString("en-GB", {
                 weekday: "long",
                 year: "numeric",
                 month: "long",
