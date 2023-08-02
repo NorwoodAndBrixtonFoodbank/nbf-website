2e9b4c02a175ae66cc7670416870052726d4e839
Varun Latthe <varun.latthe@gmail.com>
2023-08-02 09:42:51 +0100

    VFB-6: Fix icon test failure

--- src/components/Icons/SpeechBubbleIcon.tsx
+++ src/components/Icons/SpeechBubbleIcon.tsx
@@ -1,23 +1,22 @@
+"use client";
+
 import React from "react";
 import Icon from "@/components/Icons/Icon";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
-import { useTheme } from "styled-components";
+import { styled } from "styled-components";
 
 interface Props {
     onHoverText?: string;
     color?: string;
     popper?: boolean;
 }
+
+const ColouredIcon = styled(Icon)`
+    color: ${(props) => props.color ?? props.theme.accentBackgroundColor};
+`;
+
 const SpeechBubbleIcon: React.FC<Props> = (props) => {
-    const theme = useTheme();
-    return (
-        <Icon
-            icon={faComment}
-            onHoverText={props.onHoverText}
-            color={props.color ?? theme.accentBackgroundColor}
-            popper={props.popper}
-        />
-    );
+    return <ColouredIcon icon={faComment} onHoverText={props.onHoverText} popper={props.popper} />;
 };
 
 export default SpeechBubbleIcon;
