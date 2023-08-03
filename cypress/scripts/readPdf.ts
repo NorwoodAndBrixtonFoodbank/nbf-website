36d5b29fe064fae79f5fff7633930d7fa1be2c8f
paopaosoftwire <paopao.chuenchoksan@softwire.com>
2023-08-03 11:46:39 +0100

    fix lint errors

--- cypress/scripts/readPdf.ts
+++ cypress/scripts/readPdf.ts
@@ -2,10 +2,10 @@ import fs from "fs";
 import path from "path";
 import pdf from "pdf-parse";
 
-const readPdf = (pathToPdf: string) => {
+const readPdf = (pathToPdf: string): Promise<string> => {
     return new Promise((resolve) => {
         const pdfPath = path.resolve(pathToPdf);
-        let dataBuffer = fs.readFileSync(pdfPath);
+        const dataBuffer = fs.readFileSync(pdfPath);
         pdf(dataBuffer).then(function ({ text }: { text: string }) {
             resolve(text);
         });
