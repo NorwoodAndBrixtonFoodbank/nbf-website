cbc78cf90bbf0b65f5c87e4295aa237e77b8283c
Varun Latthe <varun.latthe@gmail.com>
2023-08-03 10:53:44 +0100

    VFB-20: Implement the action button

--- src/app/clients/ActionBar.cy.tsx
+++ src/app/clients/ActionBar.cy.tsx
@@ -34,88 +34,51 @@ describe("Clients - Action Bar", () => {
         cy.mount(<Component />);
     });
 
-    it("should open the menu when the status button is clicked", () => {
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-    });
+    [
+        ["status", "Request Denied"],
+        ["action", "Print Shopping List"],
+    ].forEach(([type, example]) => {
+        describe(`${type} menu`, () => {
+            it(`should open the menu when the ${type} button is clicked`, () => {
+                cy.mount(<Component />);
+                cy.get(`#${type}-button`).click();
+                cy.get(`#${type}-menu`).should("exist");
+            });
 
-    it("should update the button text when statuses are selected", () => {
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-        cy.get("#status-menu").contains("Request Denied").click();
-        cy.get("#status-modal-button").contains("Apply 1 status to 1 item");
-        cy.get(".MuiBackdrop-root").trigger("keydown", { key: "Escape", force: true });
-    });
+            it("should open the modal when an item is selected", () => {
+                cy.mount(<Component />);
+                cy.get(`#${type}-button`).click();
+                cy.get(`#${type}-menu`).should("exist");
+                cy.get(`#${type}-menu`).contains(example).click();
+                cy.get(`#${type}-modal-header`).should("exist");
+            });
 
-    it("should open the modal when the modal button is clicked when an item is selected", () => {
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-        cy.get("#status-menu").contains("Request Denied").click();
-        cy.get(".MuiBackdrop-root").trigger("keydown", { key: "Escape", force: true });
-        cy.get("#status-modal-button").click();
-        cy.get("#status-modal-header").should("exist");
-    });
+            it("should close the modal when the close button is clicked", () => {
+                cy.mount(<Component />);
+                cy.get(`#${type}-button`).click();
+                cy.get(`#${type}-menu`).should("exist");
+                cy.get(`#${type}-menu`).contains(example).click();
+                cy.get(`#${type}-modal-header`).should("exist");
+                cy.get("[aria-label='Close Button']").click();
+                cy.get(`#${type}-modal-header`).should("not.exist");
+            });
 
-    it("should not open the modal when the modal button is clicked when no items are selected", () => {
-        cy.mount(<Component />);
-        cy.get("#status-modal-button").click();
-        cy.get("#status-modal-header").should("not.exist");
-    });
-
-    it("should contain all of the selected statuses in the modal", () => {
-        const statuses = [
-            "Request Denied",
-            "Pending More Info",
-            "Ready to Dispatch",
-            "Delivery Failed",
-            "Delivery Cancelled",
-        ];
-
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-        statuses.forEach((status) => {
-            cy.get("#status-menu").contains(status).click();
+            it("should have a modal with date and time pickers", () => {
+                cy.mount(<Component />);
+                cy.get(`#${type}-button`).click();
+                cy.get(`#${type}-menu`).should("exist");
+                cy.get(`#${type}-menu`).contains(example).click();
+                cy.get(`#${type}-modal-header`).should("exist");
+                cy.get(".MuiPaper-root").contains("Date");
+                cy.get(".MuiPaper-root").contains("Time");
+                const dateString = new Date().toLocaleDateString("en-GB");
+                cy.get("input[value='" + dateString + "']").should("exist");
+                const timeString = new Date().toLocaleTimeString("en-GB", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                });
+                cy.get("input[value='" + timeString + "']").should("exist");
+            });
         });
-        cy.get(".MuiBackdrop-root").trigger("keydown", { key: "Escape", force: true });
-        cy.get("#status-modal-button").click();
-        cy.get("#status-modal-header").should("exist");
-        statuses.forEach((status) => {
-            cy.get(".MuiPaper-root").contains(status);
-        });
-    });
-
-    it("should close the modal when the close button is clicked", () => {
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-        cy.get("#status-menu").contains("Request Denied").click();
-        cy.get(".MuiBackdrop-root").trigger("keydown", { key: "Escape", force: true });
-        cy.get("#status-modal-button").click();
-        cy.get("#status-modal-header").should("exist");
-        cy.get("[aria-label='Close Button']").click();
-        cy.get("#status-modal-header").should("not.exist");
-    });
-
-    it("should have a modal with date and time pickers", () => {
-        cy.mount(<Component />);
-        cy.get("#status-button").click();
-        cy.get("#status-menu").should("exist");
-        cy.get("#status-menu").contains("Request Denied").click();
-        cy.get(".MuiBackdrop-root").trigger("keydown", { key: "Escape", force: true });
-        cy.get("#status-modal-button").click();
-        cy.get("#status-modal-header").should("exist");
-        cy.get(".MuiPaper-root").contains("Date");
-        cy.get(".MuiPaper-root").contains("Time");
-        const dateString = new Date().toLocaleDateString("en-GB");
-        cy.get("input[value='" + dateString + "']").should("exist");
-        const timeString = new Date().toLocaleTimeString("en-GB", {
-            hour: "2-digit",
-            minute: "2-digit",
-        });
-        cy.get("input[value='" + timeString + "']").should("exist");
     });
 });
--- src/app/clients/ActionBar.tsx
+++ src/app/clients/ActionBar.tsx
@@ -1,26 +1,22 @@
 "use client";
 
 import supabase, { Schema } from "@/supabase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import Button from "@mui/material/Button/Button";
 import Menu from "@mui/material/Menu/Menu";
 import MenuList from "@mui/material/MenuList/MenuList";
 import MenuItem from "@mui/material/MenuItem/MenuItem";
-import ListItemText from "@mui/material/ListItemText/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon/ListItemIcon";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import Modal from "@/components/Modal/Modal";
 import { DatePicker, TimePicker } from "@mui/x-date-pickers";
-import dayjs from "dayjs";
-import Icon from "@/components/Icons/Icon";
+import dayjs, { Dayjs } from "dayjs";
 
 interface Props {
     selected: number[];
     data: Schema["parcels"][];
 }
 
-export const statuses = [
+const statuses = [
     "Request Denied",
     // probably shouldn't be a status (?)
     "No Status",
@@ -37,6 +33,15 @@ export const statuses = [
     "Fulfilled with Trussell Trust",
 ];
 
+const actions = [
+    "Generate Map",
+    "Print Day Overview",
+    "Print Shopping List",
+    "Print Shipping Label",
+    "Print Driver Overview",
+    "Delete Request",
+];
+
 const OuterDiv = styled.div`
     display: flex;
     padding: 1rem;
@@ -46,54 +51,112 @@ const OuterDiv = styled.div`
     margin: 1rem;
 `;
 
-const Spacer = styled.div`
-    flex-grow: 1;
-    display: flex;
-`;
-
 const ActionBar: React.FC<Props> = ({ selected, data }) => {
     const selectedData = Array.from(selected.map((index) => data[index]));
 
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [selectedStatuses, setSelectedStatuses] = useState<boolean[]>(
-        Array.from(statuses.map(() => false))
-    );
+    const [statusAnchorEl, setStatusAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [actionAnchorEl, setActionAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
+    const [selectedAction, setSelectedAction] = useState<string | null>(null);
 
-    const [modal, setModal] = useState(false);
+    const [statusModal, setStatusModal] = useState(false);
+    const [actionModal, setActionModal] = useState(false);
 
-    const statusesToApply: string[] = selectedStatuses
-        .map((status, index) => {
-            if (status) {
-                return statuses[index];
-            }
-            return null;
-        })
-        .filter((status) => status !== null) as string[];
+    const [modalError, setModalError] = useState<string | null>(null);
+
+    const submitStatus = async (date: Dayjs): Promise<void> => {
+        const toInsert = selectedData
+            .map((parcel) => {
+                const event_name = selectedStatus!;
+                const parcel_id = parcel.primary_key;
+                const timestamp = date.set("second", 0).toISOString();
+                return {
+                    event_name,
+                    parcel_id,
+                    timestamp,
+                };
+            })
+            .flat();
+
+        const { error } = await supabase.from("events").insert(toInsert);
+
+        if (error) {
+            setModalError(error.message);
+        } else {
+            setStatusModal(false);
+            setModalError(null);
+        }
+    };
+
+    const submitAction = async (): Promise<void> => {};
 
     return (
         <>
-            <StatusModal
-                isOpen={modal}
-                onClose={() => setModal(false)}
+            <SharedModal
+                isOpen={statusModal}
+                onClose={() => {
+                    setStatusModal(false);
+                    setModalError(null);
+                }}
                 data={selectedData}
-                statuses={statusesToApply}
+                status={selectedStatus}
+                header="Apply Status"
+                headerId="status-modal-header"
+                onSubmit={submitStatus}
+                errorText={modalError}
             />
-            <Menu open={anchorEl !== null} onClose={() => setAnchorEl(null)} anchorEl={anchorEl}>
+            <SharedModal
+                isOpen={actionModal}
+                onClose={() => {
+                    setActionModal(false);
+                    setModalError(null);
+                }}
+                data={selectedData}
+                status={selectedAction}
+                header="Perform Action"
+                headerId="action-modal-header"
+                onSubmit={submitAction}
+                errorText={modalError}
+            />
+            <Menu
+                open={statusAnchorEl !== null}
+                onClose={() => setStatusAnchorEl(null)}
+                anchorEl={statusAnchorEl}
+            >
                 <MenuList id="status-menu">
                     {statuses.map((status, index) => {
                         return (
                             <MenuItem
                                 key={index}
                                 onClick={() => {
-                                    selectedStatuses[index] = !selectedStatuses[index];
-                                    setSelectedStatuses([...selectedStatuses]);
-                                    console.log("hi");
+                                    setSelectedStatus(status);
+                                    setStatusModal(true);
+                                    setStatusAnchorEl(null);
                                 }}
                             >
-                                <ListItemIcon>
-                                    {selectedStatuses[index] ? <Icon icon={faCheck} /> : <></>}
-                                </ListItemIcon>
-                                <ListItemText primary={status} />
+                                {status}
+                            </MenuItem>
+                        );
+                    })}
+                </MenuList>
+            </Menu>
+            <Menu
+                open={actionAnchorEl !== null}
+                onClose={() => setActionAnchorEl(null)}
+                anchorEl={actionAnchorEl}
+            >
+                <MenuList id="action-menu">
+                    {actions.map((action, index) => {
+                        return (
+                            <MenuItem
+                                key={index}
+                                onClick={() => {
+                                    setSelectedAction(action);
+                                    setActionModal(true);
+                                    setActionAnchorEl(null);
+                                }}
+                            >
+                                {action}
                             </MenuItem>
                         );
                     })}
@@ -102,33 +165,34 @@ const ActionBar: React.FC<Props> = ({ selected, data }) => {
             <OuterDiv>
                 <Button
                     variant="contained"
-                    onClick={(event) => setAnchorEl(event.currentTarget)}
+                    onClick={(event) => setStatusAnchorEl(event.currentTarget)}
                     type="button"
                     id="status-button"
                 >
                     Statuses
                 </Button>
-                <Button variant="contained">Actions</Button>
-                <Spacer />
                 <Button
-                    type="button"
                     variant="contained"
-                    onClick={() => setModal(statusesToApply.length > 0)}
-                    id="status-modal-button"
+                    onClick={(event) => setActionAnchorEl(event.currentTarget)}
+                    type="button"
+                    id="action-button"
                 >
-                    Apply {statusesToApply.length} status{statusesToApply.length === 1 ? "" : "es"}{" "}
-                    to {selectedData.length} item{selectedData.length === 1 ? "" : "s"}
+                    Actions
                 </Button>
             </OuterDiv>
         </>
     );
 };
 
-interface StatusModalProps {
+interface SharedModalProps {
     isOpen: boolean;
     onClose: () => void;
     data: Schema["parcels"][];
-    statuses: string[];
+    status: string | null;
+    onSubmit: (date: Dayjs) => void;
+    header: string;
+    headerId: string;
+    errorText: string | null;
 }
 
 const Row = styled.div`
@@ -147,45 +211,21 @@ const ModalInner = styled.div`
 const StatusText = styled.p`
     margin-left: 1rem;
     border-top: 1px solid darkgrey;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
+    padding: 1rem 0;
     &:last-child {
         border-bottom: 1px solid darkgrey;
-        padding-bottom: 1rem;
     }
 `;
 
-const StatusModal: React.FC<StatusModalProps> = (props) => {
+const SharedModal: React.FC<SharedModalProps> = (props) => {
     const [date, setDate] = useState(dayjs(Date()));
-    const [error, setError] = useState<string | null>(null);
 
-    const onClick = async (): Promise<void> => {
-        const toInsert = props.data
-            .map((parcel) => {
-                return props.statuses.map((status) => {
-                    const event_name = status;
-                    const parcel_id = parcel.primary_key;
-                    const timestamp = date.set("second", 0).toISOString();
-                    return {
-                        event_name,
-                        parcel_id,
-                        timestamp,
-                    };
-                });
-            })
-            .flat();
-
-        const response = await supabase.from("events").insert(toInsert);
-
-        if (response.error) {
-            setError(response.error.message);
-        } else {
-            props.onClose();
-        }
-    };
+    useEffect(() => {
+        setDate(dayjs(Date()));
+    }, [props.isOpen]);
 
     return (
-        <Modal {...props} header="Apply Status" headerId="status-modal-header">
+        <Modal {...props} header={props.header} headerId={props.headerId}>
             <ModalInner>
                 <Row>
                     <p>Date: </p>
@@ -219,14 +259,7 @@ const StatusModal: React.FC<StatusModalProps> = (props) => {
                 </Row>
                 <p>Applying:</p>
                 <div>
-                    {props.statuses.map((status) => {
-                        return (
-                            <StatusText key={status}>
-                                {status} to {props.data.length} item
-                                {props.data.length === 1 ? "" : "s"}
-                            </StatusText>
-                        );
-                    })}
+                    <StatusText key={props.status}>{props.status}</StatusText>
                 </div>
                 <p>To</p>
                 <div>
@@ -244,8 +277,8 @@ const StatusModal: React.FC<StatusModalProps> = (props) => {
                         );
                     })}
                 </div>
-                {error ? <small>{error}</small> : <></>}
-                <Button type="button" variant="contained" onClick={onClick}>
+                {props.errorText ? <small>{props.errorText}</small> : <></>}
+                <Button type="button" variant="contained" onClick={() => props.onSubmit(date)}>
                     Submit
                 </Button>
             </ModalInner>
