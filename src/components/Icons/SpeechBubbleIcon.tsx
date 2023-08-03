2e6b2b7aa8bccac831e87790e39d086fa72fa734
Varun Latthe <varun.latthe@gmail.com>
2023-08-03 11:16:50 +0100

    VFB-6: fix instances of "use client"

--- src/components/Icons/SpeechBubbleIcon.tsx
+++ src/components/Icons/SpeechBubbleIcon.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Icon from "@/components/Icons/Icon";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
--- src/components/Tables/FilterAccordion.tsx
+++ src/components/Tables/FilterAccordion.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Accordion, Checkbox, AccordionSummary, AccordionDetails } from "@mui/material";
 import styled from "styled-components";
