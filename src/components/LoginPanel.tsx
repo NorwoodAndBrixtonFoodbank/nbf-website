59e45f548bcb0da8a9b898a3efbd266a76b30efa
paopaosoftwire <paopao.chuenchoksan@softwire.com>
2023-08-14 12:31:19 +0100

    Change to @ import

--- src/components/LoginPanel.tsx
+++ src/components/LoginPanel.tsx
@@ -7,7 +7,7 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import React, { useEffect } from "react";
 import styled, { useTheme } from "styled-components";
 import NoSsr from "@mui/material/NoSsr";
-import Title from "./Title/Title";
+import Title from "@/components/Title/Title";
 
 export const LoginMain = styled.main`
     height: 85vh;
