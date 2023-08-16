877c32865e1b12eb81f54d8cb545cbbde4fe3dc9
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-08-16 15:48:02 +0100

    Throw error when wrong location is input

--- src/components/DayOverview/DayOverviewButton.tsx
+++ src/components/DayOverview/DayOverviewButton.tsx
@@ -37,7 +37,7 @@ const collectionCentreToAbbreviation = (
         case "Delivery":
             return "Delivery";
         default:
-            return "-";
+            throw new Error("Invalid location");
     }
 };
 
