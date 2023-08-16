bd5ed42814f7a4cf7a571536db2bfbab532f2469
daniel-conlan-softwire <Daniel.Conlan@softwire.com>
2023-08-16 11:48:23 +0100

    Removed @ import for supabase in edge function.

--- supabase/functions/admin-get-users/index.ts
+++ supabase/functions/admin-get-users/index.ts
@@ -5,13 +5,21 @@ import { type Handler, serve } from "https://deno.land/std@0.168.0/http/server.t
 import { createClient } from "https://esm.sh/@supabase/supabase-js@latest";
 import { corsHeaders } from "../_shared/cors.ts";
 
-import supabase from "@/supabase";
-
 serve(async (req: Handler): Promise<Response> => {
     const generateHeaders = (status: number): any => ({
         ...corsHeaders,
         "Content-Type": "application/json",
         status,
+    });
+
+    const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
+    const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY");
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+        global: {
+            headers: {
+                Authorization: req.headers.get("Authorization")!,
+            },
+        },
     });
 
     // -- Prevent Non-Admin Access -- //
@@ -40,9 +48,7 @@ serve(async (req: Handler): Promise<Response> => {
         return new Response("ok", { headers: corsHeaders });
     }
 
-    const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
     const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"); // TODO ADD KEY
-
     const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
         // TODO What does this do?
         global: {
