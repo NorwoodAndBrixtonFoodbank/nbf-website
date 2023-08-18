d5da995473335555322766bc1d434c7700aee7ee
SamYap-Softwire <samyun.yap@softwire.com>
2023-08-18 15:47:02 +0100

    Rebase main and resolve conflicts

--- src/app/clients/add/databaseFunctions.ts
+++ src/app/clients/add/databaseFunctions.ts
@@ -1,6 +1,7 @@
-import supabase, { InsertSchema, Schema, UpdateSchema } from "@/supabase";
 import { checkboxGroupToArray, Fields, Person } from "@/components/Form/formFunctions";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import { InsertSchema, Schema, UpdateSchema } from "@/database_utils";
+import supabase from "@/supabaseServer";
 
 type FamilyDatabaseInsertRecord = InsertSchema["families"];
 type FamilyDatabaseUpdateRecord = UpdateSchema["families"];
