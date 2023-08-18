a3752bac74bf2271b53fa4e88c8d5aaed6fc9959
SamYap-Softwire <samyun.yap@softwire.com>
2023-08-18 17:43:11 +0100

    View Client Page

---
+++ src/app/clients/[id]/viewClientPage.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import LinkButton from "@/components/Buttons/LinkButton";
+import styled from "styled-components";
+import { CenterComponent, StyledCard } from "@/components/Form/formStyling";
+import { formatCamelCaseKey } from "@/app/clients/add/databaseFunctions";
+import { ClientData } from "@/app/clients/[id]/getClientData";
+
+// TODO VFB-24: Formatting - overflow (  word-break: break-word; ) ?
+
+interface Props {
+    clientID: string;
+    clientData: ClientData;
+}
+
+const NormalText = styled.pre`
+    font-family: inherit;
+    font-weight: normal;
+    padding-bottom: 1rem;
+    width: inherit;
+    word-break: break-word;
+`;
+
+const TempDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    place-items: baseline;
+    width: inherit;
+`;
+
+const KeyText = styled.p`
+    font-weight: bold;
+`;
+
+const ViewClientPage: React.FC<Props> = ({ clientID, clientData }) => {
+    return (
+        <CenterComponent>
+            <StyledCard>
+                <TempDiv>
+                    {Object.entries(clientData).map(([objectKey, objectValue], index) => (
+                        <TempDiv key={index}>
+                            <KeyText>{formatCamelCaseKey(objectKey)}</KeyText>
+                            <NormalText>{objectValue as string}</NormalText>
+                        </TempDiv>
+                    ))}
+                    <LinkButton link={`/clients/add/${clientID}`}>Edit</LinkButton>
+                    <LinkButton link={`/parcels/add/${clientID}`}>Continue</LinkButton>
+                    <LinkButton link="/clients">Back</LinkButton>
+                </TempDiv>
+            </StyledCard>
+        </CenterComponent>
+    );
+};
+
+export default ViewClientPage;
