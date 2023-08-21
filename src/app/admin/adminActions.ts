2c1eafc2b8eea94159c1da402ed84d8e7c7b6de7
Varun Latthe <varun.latthe@gmail.com>
2023-08-21 14:52:22 +0100

    VFB-31: undo some changes

--- src/app/admin/adminActions.ts
+++ src/app/admin/adminActions.ts
@@ -3,39 +3,8 @@
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { FunctionsResponse } from "@supabase/functions-js/src/types";
-import { User } from "@supabase/gotrue-js";
-import { Database } from "@/database_types_file";
 
 const supabase = createServerActionClient({ cookies });
-
-export interface UserRow {
-    id: string;
-    email: string;
-    userRole: Database["public"]["Enums"]["role"];
-    createdAt: number;
-    updatedAt: number;
-}
-
-export const getUsers = async (): Promise<UserRow[]> => {
-    const { data, error } = await supabase.functions.invoke("admin-get-users");
-
-    // TODO VFB-23 Move error handling of this request to client side
-    if (error) {
-        throw new Error("Unable to fetch the user information.");
-    }
-
-    const users: User[] = JSON.parse(data);
-
-    return users.map((user: User) => {
-        return {
-            id: user.id,
-            email: user.email ?? "-",
-            userRole: user.app_metadata.role,
-            createdAt: Date.parse(user.created_at),
-            updatedAt: Date.parse(user.updated_at ?? ""),
-        };
-    });
-};
 
 export const createUser = async (fields: any): Promise<FunctionsResponse<any>> => {
     return await supabase.functions.invoke("admin-create-user", {
