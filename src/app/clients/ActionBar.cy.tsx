6ee4e15739e5a9648a7410d8b718915bad65949b
Varun Latthe <varun.latthe@gmail.com>
2023-08-22 10:32:17 +0100

    Change action tests to only select one parcel

--- src/app/clients/ActionBar.cy.tsx
+++ src/app/clients/ActionBar.cy.tsx
@@ -10,115 +10,210 @@ describe("Clients - Action Bar", () => {
             primaryKey: "primaryKey1",
             addressPostcode: "AB1 2CD",
             collectionCentre: "Centre 1",
-            collectionDatetime: new Date().toISOString(),
             congestionChargeApplies: false,
+            collectionDatetime: new Date().toISOString(),
             familyCategory: "Family 1",
-            flaggedForAttention: false,
             fullName: "John Smith",
-            lastStatusUpdate: new Date().toISOString(),
             lastStatus: "Delivered",
             packingDatetime: new Date().toISOString(),
             packingTimeLabel: "10:00",
             parcelId: "123456789",
             requiresFollowUpPhoneCall: false,
+            flaggedForAttention: false,
+            voucherNumber: "123456789",
         },
         {
             primaryKey: "primaryKey2",
             addressPostcode: "AB1 aaaa2CD",
             collectionCentre: "Centraaaae 1",
+            congestionChargeApplies: true,
             collectionDatetime: new Date().toISOString(),
-            congestionChargeApplies: false,
             familyCategory: "Familaaaay 1",
-            flaggedForAttention: false,
             fullName: "John Smaaaaith",
-            lastStatusUpdate: new Date().toISOString(),
             lastStatus: "Deliveaaaared",
             packingDatetime: new Date().toISOString(),
             packingTimeLabel: "10aaaa:00",
             parcelId: "123456aaaa789",
             requiresFollowUpPhoneCall: false,
+            flaggedForAttention: false,
+            voucherNumber: "123456aaaa789",
         },
     ];
 
-    const selectedIndices = [0, 1];
+    interface Props {
+        selected: number[];
+    }
 
-    const MockActionBar: React.FC<{}> = () => {
+    const MockActionBar: React.FC<Props> = ({ selected }) => {
         return (
             <Localization>
                 <StyleManager>
-                    <ActionBar data={mockData} selected={selectedIndices} />
+                    <ActionBar {...{ data: mockData, selected }} />
                 </StyleManager>
             </Localization>
         );
     };
+    describe("Statuses", () => {
+        const selectedIndices = [0, 1];
 
-    beforeEach(() => {
-        cy.mount(<MockActionBar />);
+        beforeEach(() => {
+            cy.mount(<MockActionBar selected={selectedIndices} />);
+        });
+
+        it("should open the status menu when the status button is clicked", () => {
+            cy.get("#status-button").click();
+            cy.get("#status-menu").should("exist");
+        });
+
+        it("should open the modal when an item is selected", () => {
+            cy.get("#status-button").click();
+            cy.get("#status-menu").should("exist");
+            cy.get("#status-menu").contains("Request Denied").click();
+            cy.get("#status-modal-header").should("exist");
+        });
+
+        it("should close the modal when the close button is clicked", () => {
+            cy.get("#status-button").click();
+            cy.get("#status-menu").should("exist");
+            cy.get("#status-menu").contains("Request Denied").click();
+            cy.get("#status-modal-header").should("exist");
+            cy.get("[aria-label='Close Button']").click();
+            cy.get("#status-modal-header").should("not.exist");
+        });
+
+        it("should have a modal with date and time pickers", () => {
+            cy.get("#status-button").click();
+            cy.get("#status-menu").should("exist");
+            cy.get("#status-menu").contains("Request Denied").click();
+            cy.get("#status-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+        });
+
+        it("should have a modal that contains the selected data", () => {
+            cy.get("#status-button").click();
+            cy.get("#status-menu").should("exist");
+            cy.get("#status-menu").contains("Request Denied").click();
+            cy.get("#status-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+            selectedIndices.forEach((index) => {
+                cy.get(".MuiPaper-root").contains(mockData[index].collectionCentre);
+                cy.get(".MuiPaper-root").contains(mockData[index].fullName);
+            });
+        });
     });
 
-    it("should render", () => {});
+    describe("Actions", () => {
+        const row = mockData[0];
 
-    [
-        ["status", "Request Denied"],
-        ["action", "Print Shopping List"],
-    ].forEach(([type, example]) => {
-        describe(`${type} menu`, () => {
-            it(`should open the menu when the ${type} button is clicked`, () => {
-                cy.get(`#${type}-button`).click();
-                cy.get(`#${type}-menu`).should("exist");
-            });
+        beforeEach(() => {
+            cy.mount(<MockActionBar selected={[0]} />);
+        });
 
-            it("should open the modal when an item is selected", () => {
-                cy.get(`#${type}-button`).click();
-                cy.get(`#${type}-menu`).should("exist");
-                cy.get(`#${type}-menu`).contains(example).click();
-                cy.get(`#${type}-modal-header`).should("exist");
-            });
+        it("should open the action menu when the action button is clicked", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+        });
 
-            it("should close the modal when the close button is clicked", () => {
-                cy.get(`#${type}-button`).click();
-                cy.get(`#${type}-menu`).should("exist");
-                cy.get(`#${type}-menu`).contains(example).click();
-                cy.get(`#${type}-modal-header`).should("exist");
-                cy.get("[aria-label='Close Button']").click();
-                cy.get(`#${type}-modal-header`).should("not.exist");
-            });
+        it("should open the modal when an item is selected", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+        });
 
-            it("should have a modal with date and time pickers", () => {
-                cy.get(`#${type}-button`).click();
-                cy.get(`#${type}-menu`).should("exist");
-                cy.get(`#${type}-menu`).contains(example).click();
-                cy.get(`#${type}-modal-header`).should("exist");
-                cy.get(".MuiPaper-root").contains("Date");
-                cy.get(".MuiPaper-root").contains("Time");
-                const dateString = new Date().toLocaleDateString("en-GB");
-                cy.get(`input[value="${dateString}"]`).should("exist");
-                const timeString = new Date().toLocaleTimeString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                });
-                cy.get(`input[value="${timeString}"]`).should("exist");
-            });
+        it("should close the modal when the close button is clicked", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+            cy.get("[aria-label='Close Button']").click();
+            cy.get("#action-modal-header").should("not.exist");
+        });
 
-            it("should have a modal that contains the selected data", () => {
-                cy.get(`#${type}-button`).click();
-                cy.get(`#${type}-menu`).should("exist");
-                cy.get(`#${type}-menu`).contains(example).click();
-                cy.get(`#${type}-modal-header`).should("exist");
-                cy.get(".MuiPaper-root").contains("Date");
-                cy.get(".MuiPaper-root").contains("Time");
-                const dateString = new Date().toLocaleDateString("en-GB");
-                cy.get(`input[value="${dateString}"]`).should("exist");
-                const timeString = new Date().toLocaleTimeString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                });
-                cy.get(`input[value="${timeString}"]`).should("exist");
-                selectedIndices.forEach((index) => {
-                    cy.get(".MuiPaper-root").contains(mockData[index].collectionCentre);
-                    cy.get(".MuiPaper-root").contains(mockData[index].fullName);
-                });
+        it("should have a modal with date and time pickers", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
             });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+        });
+
+        it("should have a modal that contains the selected data", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+            cy.get(".MuiPaper-root").contains(row.collectionCentre);
+            cy.get(".MuiPaper-root").contains(row.fullName);
+        });
+
+        it("should have a modal that contains the selected data", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+            cy.get(".MuiPaper-root").contains(row.collectionCentre);
+            cy.get(".MuiPaper-root").contains(row.fullName);
+        });
+
+        it("should have a modal that contains the selected data", () => {
+            cy.get("#action-button").click();
+            cy.get("#action-menu").should("exist");
+            cy.get("#action-menu").contains("Print Shopping List").click();
+            cy.get("#action-modal-header").should("exist");
+            cy.get(".MuiPaper-root").contains("Date");
+            cy.get(".MuiPaper-root").contains("Time");
+            const dateString = new Date().toLocaleDateString("en-GB");
+            cy.get(`input[value="${dateString}"]`).should("exist");
+            const timeString = new Date().toLocaleTimeString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            cy.get(`input[value="${timeString}"]`).should("exist");
+            cy.get(".MuiPaper-root").contains(row.collectionCentre);
+            cy.get(".MuiPaper-root").contains(row.fullName);
         });
     });
 });
