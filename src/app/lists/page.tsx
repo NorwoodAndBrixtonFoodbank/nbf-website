05507a54b06b03c3dc913d995c539ef11717dbd5
Lincoln Stone <lincoln.stone@softwire.com>
2024-01-21 23:20:46 +0000

    VFB-6: Fix structure of Lists page to avoid hydration errors

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,30 +1,13 @@
-import { fetchComment, fetchLists } from "@/common/fetch";
-import { Schema } from "@/databaseUtils";
 import { Metadata } from "next";
 import React from "react";
-import ListsDataView from "@/app/lists/ListDataview";
 import Title from "@/components/Title/Title";
-import supabase from "@/supabaseServer";
+import ListsPage from "./ListsPage";
 
-// disables caching
-export const revalidate = 0;
-
-interface Props {
-    data: Schema["lists"][];
-    comment: string;
-}
-
-const fetchData = async (): Promise<Props> => {
-    const [data, comment] = await Promise.all([fetchLists(supabase), fetchComment(supabase)]);
-    return { data, comment };
-};
-
-const Lists = async (): Promise<React.ReactElement> => {
-    const { data, comment } = await fetchData();
+const Lists: () => Promise<React.ReactElement> = async () => {
     return (
         <main>
             <Title>Lists</Title>
-            <ListsDataView data={data} comment={comment} />
+            <ListsPage />
         </main>
     );
 };
