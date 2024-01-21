2cb87fe5f0f25c8ddae9e5a56ef4f12b30f0224a
Lincoln Stone <lincoln.stone@softwire.com>
2024-01-21 23:20:46 +0000

    VFB-6: Fix structure of Lists page to avoid hydration errors

---
+++ src/app/lists/ListsPage.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Schema } from "@/databaseUtils";
+import supabase from "@/supabaseClient";
+import { fetchComment, fetchLists } from "@/common/fetch";
+import ListsDataView from "@/app/lists/ListDataview";
+
+interface FetchedListsData {
+    data: Schema["lists"][];
+    comment: string;
+}
+
+const fetchData = async (): Promise<FetchedListsData> => {
+    const [data, comment] = await Promise.all([fetchLists(supabase), fetchComment(supabase)]);
+    return { data, comment };
+};
+
+const ListsPage: React.FC<{}> = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [listsData, setListsData] = useState<Schema["lists"][]>([]);
+    const [comment, setComment] = useState("");
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            const fetchedData = await fetchData();
+            setListsData(fetchedData.data);
+            setComment(fetchedData.comment);
+            setIsLoading(false);
+        })();
+    }, []);
+
+    return isLoading ? <></> : <ListsDataView data={listsData} comment={comment} />;
+};
+
+export default ListsPage;
