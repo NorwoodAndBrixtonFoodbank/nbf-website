bc254eb69fb0b154108a394eb686783f9169ed70
MTHarkins <michael.harkins@softwire.com>
2024-03-05 13:24:16 +0000

    Formatting

--- src/app/lists/CommentBox.tsx
+++ src/app/lists/CommentBox.tsx
@@ -3,7 +3,7 @@
 import FreeFormTextInput from "@/components/DataInput/FreeFormTextInput";
 import supabase from "@/supabaseClient";
 import Button from "@mui/material/Button";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 interface CommentProps {
