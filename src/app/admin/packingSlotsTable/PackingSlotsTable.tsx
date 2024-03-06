7cba2fc0ef614441c1563835508829f2a1ea07d5
minalui <mina.lui@softwire.com>
2024-03-06 16:10:17 +0000

    VFB-117: Create new row on click of add new slot button

--- src/app/admin/packingSlotsTable/PackingSlotsTable.tsx
+++ src/app/admin/packingSlotsTable/PackingSlotsTable.tsx
@@ -2,14 +2,54 @@
 
 import React, { useEffect } from "react";
 import { Schema } from "@/databaseUtils";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+    DataGrid,
+    GridColDef,
+    GridRowModes,
+    GridRowModesModel,
+    GridRowsProp,
+    GridToolbarContainer,
+} from "@mui/x-data-grid";
 import { Switch } from "@mui/material";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 interface Props {
     packingSlotsData: Schema["packing_slots"][];
 }
 
+interface EditToolbarProps {
+    setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+    setRowModesModel: (newModel: (oldModel: GridRowModesModel) => GridRowModesModel) => void;
+    nextId: number;
+}
+
+function EditToolbar(props: EditToolbarProps): React.JSX.Element {
+    const { setRows, setRowModesModel, nextId } = props;
+
+    const handleClick = (): void => {
+        //how to pass in the next id?
+        const id = nextId;
+        setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
+        setRowModesModel((oldModel) => ({
+            ...oldModel,
+            [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
+        }));
+    };
+
+    return (
+        <GridToolbarContainer>
+            <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+                Add record
+            </Button>
+        </GridToolbarContainer>
+    );
+}
+
 const PackingSlotsTable: React.FC<Props> = (props) => {
+    const [rows, setRows] = React.useState(props.packingSlotsData);
+    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
+
     const packingSlotsTableHeaderKeysAndLabels: GridColDef[] = [
         { field: "name", headerName: "Slot Name", flex: 1, editable: true },
         {
@@ -24,30 +64,25 @@ const PackingSlotsTable: React.FC<Props> = (props) => {
                 }
             },
         },
-        { field: "order", headerName: "Order (1 is on Top)", flex: 1, editable: true },
+        { field: "order", headerName: "Order (Smallest number on top)", flex: 1, editable: true },
     ];
 
-    const initialRows = [
-        {
-            id: 1,
-            name: "AM",
-            is_hidden: false,
-            order: 0,
-        },
-        {
-            id: 2,
-            name: "PM",
-            is_hidden: false,
-            order: 1,
-        },
-    ];
+    const nextId = props.packingSlotsData.length + 1;
 
-    useEffect(() => {
-        console.log(props.packingSlotsData);
-    }, []);
     return (
         <>
-            <DataGrid rows={initialRows} columns={packingSlotsTableHeaderKeysAndLabels} />
+            <DataGrid
+                rows={rows}
+                columns={packingSlotsTableHeaderKeysAndLabels}
+                editMode="row"
+                rowModesModel={rowModesModel}
+                slots={{
+                    toolbar: EditToolbar,
+                }}
+                slotProps={{
+                    toolbar: { setRows, setRowModesModel, nextId },
+                }}
+            />
         </>
     );
 };
