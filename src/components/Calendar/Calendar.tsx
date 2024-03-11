60959301accd4d519617a02c329ab509dc8cdeab
MTHarkins <michael.harkins@softwire.com>
2024-03-11 14:27:05 +0000

    Rename setListOfIngridients prop

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -97,6 +97,7 @@ const CalendarStyling = styled(Paper)`
     .fc-timeGridWeek-view .fc-event-title-container {
         width: 100%;
         height: 100%;
+        scrollto
     }
 
     .fc-timeGridWeek-view .fc-event-title,
