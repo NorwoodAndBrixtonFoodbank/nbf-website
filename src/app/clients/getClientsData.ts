0c7fa686e2657be8179c956ab852b302aa5688d3
minalui <mina.lui@softwire.com>
2024-03-15 16:41:32 +0000

    VFB-57: Error log getClientsData.ts

--- src/app/clients/getClientsData.ts
+++ src/app/clients/getClientsData.ts
@@ -3,7 +3,7 @@ import { familyCountToFamilyCategory } from "@/app/parcels/getExpandedParcelDeta
 import { DatabaseError } from "@/app/errorClasses";
 import { Supabase } from "@/supabaseUtils";
 import { logError } from "@/logger/logger";
-import { v4 as uuidv4 } from "uuid";
+
 const getClientsData = async (supabase: Supabase): Promise<ClientsTableRow[]> => {
     const data: ClientsTableRow[] = [];
 
@@ -13,39 +13,33 @@ const getClientsData = async (supabase: Supabase): Promise<ClientsTableRow[]> =>
         .order("full_name");
 
     if (clientError) {
-        const id = uuidv4();
-        const meta = {
-            error: clientError,
-            id: id,
-            location: "app/clients/getClientsData.ts",
-        };
-        void logError("Error with fetch: clients", meta);
-        throw new DatabaseError("fetch", "clients");
+        const response = logError("Error with fetch: Clients", clientError);
+        response.then((errorId) => {
+            throw new DatabaseError("fetch", "clients", errorId);
+        });
     }
 
-    for (const client of clients) {
-        const { count, error: familyError } = await supabase
-            .from("families")
-            .select("*", { count: "exact", head: true })
-            .eq("family_id", client.family_id);
+    if (clients) {
+        for (const client of clients) {
+            const { count, error: familyError } = await supabase
+                .from("families")
+                .select("*", { count: "exact", head: true })
+                .eq("family_id", client.family_id);
 
-        if (familyError || count === null) {
-            const id = uuidv4();
-            const meta = {
-                error: familyError,
-                id: id,
-                location: "app/clients/getClientsData.ts",
-            };
-            void logError("Error with fetch: families", meta);
-            throw new DatabaseError("fetch", "families");
+            if (familyError || count === null) {
+                const response = logError("Error with fetch: Client families", familyError);
+                response.then((errorId) => {
+                    throw new DatabaseError("fetch", "client families", errorId);
+                });
+            } else {
+                data.push({
+                    clientId: client.primary_key,
+                    fullName: client.full_name,
+                    familyCategory: familyCountToFamilyCategory(count),
+                    addressPostcode: client.address_postcode,
+                });
+            }
         }
-
-        data.push({
-            clientId: client.primary_key,
-            fullName: client.full_name,
-            familyCategory: familyCountToFamilyCategory(count),
-            addressPostcode: client.address_postcode,
-        });
     }
 
     return data;
