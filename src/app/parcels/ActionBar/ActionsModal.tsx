d25a1d877c89ed91c78c0c4580150ae447882c63
minalui <mina.lui@softwire.com>
2024-03-18 10:37:19 +0000

    VFB-57: Change error log ActionsModal.tsx

--- src/app/parcels/ActionBar/ActionsModal.tsx
+++ src/app/parcels/ActionBar/ActionsModal.tsx
@@ -13,7 +13,6 @@ import supabase from "@/supabaseClient";
 import { DatabaseError } from "@/app/errorClasses";
 import { statusType } from "./Statuses";
 import SelectedParcelsOverview from "./SelectedParcelsOverview";
-import { v4 as uuidv4 } from "uuid";
 import { logError } from "@/logger/logger";
 
 export type ActionType = "pdfDownload" | "deleteParcel" | "generateMap";
@@ -138,14 +137,10 @@ export const DayOverviewInput: React.FC<DayOverviewInputProps> = ({
                 .from("collection_centres")
                 .select("primary_key, name");
             if (error) {
-                const id = uuidv4();
-                const meta = {
-                    error: error,
-                    id: id,
-                    location: "app/parcels/ActionBar/ActionsModal.tsx",
-                };
-                void logError("Error with fetch: Collection centres", meta);
-                throw new DatabaseError("fetch", "collection centres");
+                const response = logError("Error with fetch: Collection centres", error);
+                response.then((errorId) => {
+                    throw new DatabaseError("fetch", "collection centres", errorId);
+                });
             }
 
             const transformedData: [string, string][] = data.map((item) => [
