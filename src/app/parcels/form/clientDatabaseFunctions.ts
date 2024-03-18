66f9f778122cf5108a3399c539ca5452e9eb9e05
minalui <mina.lui@softwire.com>
2024-03-18 14:08:56 +0000

    VFB-57: Remove unnecessary return and change error log clientDatabaseFunctions.ts

--- src/app/parcels/form/clientDatabaseFunctions.ts
+++ src/app/parcels/form/clientDatabaseFunctions.ts
@@ -1,58 +1,35 @@
 import supabase from "@/supabaseClient";
-import { InsertSchema, Schema, UpdateSchema } from "@/databaseUtils";
+import { InsertSchema, UpdateSchema } from "@/databaseUtils";
 import { DatabaseError } from "@/app/errorClasses";
-import { v4 as uuidv4 } from "uuid";
 import { logErrorReturnLogId } from "@/logger/logger";
 
 type ParcelDatabaseInsertRecord = InsertSchema["parcels"];
 type ParcelDatabaseUpdateRecord = UpdateSchema["parcels"];
-type ParcelAndClientIds = Pick<Schema["parcels"], "primary_key" | "client_id">;
 
-export const insertParcel = async (
-    parcelRecord: ParcelDatabaseInsertRecord
-): Promise<ParcelAndClientIds> => {
-    const {
-        data: ids,
-        status,
-        error,
-    } = await supabase.from("parcels").insert(parcelRecord).select("primary_key, client_id");
+export const insertParcel = async (parcelRecord: ParcelDatabaseInsertRecord): Promise<void> => {
+    const { error } = await supabase
+        .from("parcels")
+        .insert(parcelRecord)
+        .select("primary_key, client_id");
 
-    if (error === null && Math.floor(status / 100) === 2) {
-        return ids![0];
+    if (error) {
+        const logId = await logErrorReturnLogId("Error with insert: parcel data", error);
+        throw new DatabaseError("insert", "parcel data", logId);
     }
-    const id = uuidv4();
-    const meta = {
-        error: error,
-        id: id,
-        location: "app/parcels/form/clientDatabaseFunctions.ts",
-    };
-    void logErrorReturnLogId("Error with insert: parcel data", meta);
-    throw new DatabaseError("insert", "parcel data");
 };
 
 export const updateParcel = async (
     parcelRecord: ParcelDatabaseUpdateRecord,
     primaryKey: string
-): Promise<ParcelAndClientIds> => {
-    const {
-        data: ids,
-        status,
-        error,
-    } = await supabase
+): Promise<void> => {
+    const { error } = await supabase
         .from("parcels")
         .update(parcelRecord)
         .eq("primary_key", primaryKey)
         .select("primary_key, client_id");
 
-    if (error === null && Math.floor(status / 100) === 2) {
-        return ids![0];
+    if (error) {
+        const logId = await logErrorReturnLogId("Error with update: parcel data", error);
+        throw new DatabaseError("update", "parcel data", logId);
     }
-    const id = uuidv4();
-    const meta = {
-        error: error,
-        id: id,
-        location: "app/parcels/form/clientDatabaseFunctions.ts",
-    };
-    void logErrorReturnLogId("Error with update: parcel data", meta);
-    throw new DatabaseError("update", "parcel data");
 };
