3d34975e33689f7296c007f93a11b85e4e442f41
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-03-20 16:11:47 +0000

    relevant icon for website data table in admin panel

--- src/app/admin/AdminPage.tsx
+++ src/app/admin/AdminPage.tsx
@@ -14,6 +14,7 @@ import {
     faBuildingCircleArrowRight,
     faBoxOpen,
     IconDefinition,
+    faServer
 } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -70,7 +71,7 @@ const AdminPage: React.FC<Props> = (props) => {
         },
         {
             panelTitle: "Edit Website Data",
-            panelIcon: faBoxOpen, //change
+            panelIcon: faServer,
             panelContent: <WebsiteDataTable />,
         },
     ];
