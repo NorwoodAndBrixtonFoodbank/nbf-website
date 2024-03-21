3b705292002958541161155e849e3eb45158e006
minalui <mina.lui@softwire.com>
2024-03-21 14:59:07 +0000

    VFB-125: Add profile rows for admin and caller roles

--- supabase/tools/createAdminAndCallerUsers.ts
+++ supabase/tools/createAdminAndCallerUsers.ts
@@ -11,17 +11,36 @@ type Role = "admin" | "caller";
 async function createAdminAndCallerUsers(): Promise<void> {
     const supabase = getLocalSupabaseClient();
 
-    await createUser(supabase, "admin@example.com", "admin123", "admin");
-    await createUser(supabase, "caller@example.com", "caller123", "caller");
+    await createUser(
+        supabase,
+        "admin@example.com",
+        "admin123",
+        "admin",
+        "admin",
+        "person",
+        "0777777777777"
+    );
+    await createUser(
+        supabase,
+        "caller@example.com",
+        "caller123",
+        "caller",
+        "caller",
+        "person",
+        "078888888888"
+    );
 }
 
 async function createUser(
     supabase: SupabaseClient,
     email: string,
     password: string,
-    role: Role
+    role: Role,
+    firstName: string,
+    lastName: string,
+    telephoneNumber: string
 ): Promise<void> {
-    const { error } = await supabase.auth.admin.createUser({
+    const { data, error } = await supabase.auth.admin.createUser({
         email: email,
         password: password,
         email_confirm: true,
@@ -34,6 +53,19 @@ async function createUser(
     if (error) {
         console.log(error);
         throw new Error(`Failed to create ${role} user: ${email}. ${error}`);
+    }
+
+    if (data) {
+        const { error } = await supabase.from("profiles").insert({
+            primary_key: data.user?.id,
+            role: role,
+            first_name: firstName,
+            last_name: lastName,
+            telephone_number: telephoneNumber,
+        });
+        if (error) {
+            console.log(error);
+        }
     }
 
     console.log(`Created a ${role} user: ${email} (${password})`);
