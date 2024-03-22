485b9aacba95ad9f5330f0f0f87fb00c37257f49
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-03-22 12:28:14 +0000

    Fetch client details properly in edit client form

--- src/app/clients/edit/[id]/page.tsx
+++ src/app/clients/edit/[id]/page.tsx
@@ -1,23 +1,40 @@
-import { Metadata } from "next";
+"use client";
 
-import React from "react";
-import { getSupabaseServerComponentClient } from "@/supabaseServer";
+import React, { useEffect, useState } from "react";
+import supabase from "@/supabaseClient";
 import ClientForm from "@/app/clients/form/ClientForm";
 import { Errors, FormErrors } from "@/components/Form/formFunctions";
 import autofill from "@/app/clients/edit/[id]/autofill";
 import { fetchClient, fetchFamily } from "@/common/fetch";
+import { Schema } from "@/databaseUtils";
 
 interface EditClientsParameters {
     params: { id: string };
 }
 
-const EditClients: ({ params }: EditClientsParameters) => Promise<React.ReactElement> = async ({
+const EditClients: ({ params }: EditClientsParameters) => React.ReactElement = ({
     params,
 }: EditClientsParameters) => {
-    const supabase = getSupabaseServerComponentClient();
-    const clientData = await fetchClient(params.id, supabase);
-    const familyData = await fetchFamily(clientData.family_id, supabase);
-    const initialFields = autofill(clientData, familyData);
+    const [clientData, setClientData] = useState<Schema["clients"]>();
+    const [familyData, setFamilyData] = useState<Schema["families"][]>();
+    useEffect(() => {
+        (async () => {
+            if (params.id) {
+                setClientData(await fetchClient(params.id, supabase));
+            }
+        })();
+    }, [params.id]);
+    useEffect(() => {
+        (async () => {
+            if (clientData?.family_id) {
+                setFamilyData(await fetchFamily(clientData.family_id, supabase));
+            }
+        })();
+    }, [clientData?.family_id]);
+
+    console.log(clientData, familyData);
+    const initialFields = clientData && familyData ? autofill(clientData, familyData) : null;
+
     const initialFormErrors: FormErrors = {
         fullName: Errors.none,
         phoneNumber: Errors.none,
@@ -29,18 +46,16 @@ const EditClients: ({ params }: EditClientsParameters) => Promise<React.ReactEle
     };
     return (
         <main>
-            <ClientForm
-                initialFields={initialFields}
-                initialFormErrors={initialFormErrors}
-                editMode
-                clientID={params.id}
-            />
+            {initialFields && (
+                <ClientForm
+                    initialFields={initialFields}
+                    initialFormErrors={initialFormErrors}
+                    editMode
+                    clientID={params.id}
+                />
+            )}
         </main>
     );
-};
-
-export const metadata: Metadata = {
-    title: "Edit Clients",
 };
 
 export default EditClients;
