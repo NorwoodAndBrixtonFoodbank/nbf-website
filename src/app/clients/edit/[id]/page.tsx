99f12d03a85a790c21da1ffe4b4648260ce94d6e
AmeliaIsCoding <138508884+AmeliaIsCoding@users.noreply.github.com>
2024-03-25 17:25:12 +0000

    VFB-145 fix open client modal after editing client (#174)

--- src/app/clients/edit/[id]/page.tsx
+++ src/app/clients/edit/[id]/page.tsx
@@ -1,23 +1,58 @@
-import { Metadata } from "next";
+"use client";
 
-import React from "react";
-import { getSupabaseServerComponentClient } from "@/supabaseServer";
+import React, { useEffect, useState } from "react";
+import supabase from "@/supabaseClient";
 import ClientForm from "@/app/clients/form/ClientForm";
 import { Errors, FormErrors } from "@/components/Form/formFunctions";
 import autofill from "@/app/clients/edit/[id]/autofill";
 import { fetchClient, fetchFamily } from "@/common/fetch";
+import { Schema } from "@/databaseUtils";
+import { logErrorReturnLogId } from "@/logger/logger";
+import { useRouter } from "next/navigation";
+import ErrorPage from "@/app/error";
 
 interface EditClientsParameters {
     params: { id: string };
 }
 
-const EditClients: ({ params }: EditClientsParameters) => Promise<React.ReactElement> = async ({
+const EditClients: ({ params }: EditClientsParameters) => React.ReactElement = ({
     params,
 }: EditClientsParameters) => {
-    const supabase = getSupabaseServerComponentClient();
-    const clientData = await fetchClient(params.id, supabase);
-    const familyData = await fetchFamily(clientData.family_id, supabase);
-    const initialFields = autofill(clientData, familyData);
+    const [clientData, setClientData] = useState<Schema["clients"] | null>(null);
+    const [familyData, setFamilyData] = useState<Schema["families"][] | null>(null);
+    const [error, setError] = useState<Error | null>();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (params.id) {
+            setError(null);
+            fetchClient(params.id, supabase)
+                .then((client) => {
+                    setClientData(client);
+                })
+                .catch((error) => {
+                    setError({ name: error, message: "Unable to fetch client data" });
+                    logErrorReturnLogId("error fetching client data in client edit modal");
+                });
+        }
+    }, [params.id]);
+
+    useEffect(() => {
+        if (clientData?.family_id) {
+            setError(null);
+            fetchFamily(clientData.family_id, supabase)
+                .then((family) => {
+                    setFamilyData(family);
+                })
+                .catch((error) => {
+                    setError({ name: error, message: "Unable to fetch family data" });
+                    logErrorReturnLogId("error fetching family data in client edit modal");
+                });
+        }
+    }, [clientData?.family_id]);
+
+    const initialFields = clientData && familyData ? autofill(clientData, familyData) : null;
+
     const initialFormErrors: FormErrors = {
         fullName: Errors.none,
         phoneNumber: Errors.none,
@@ -29,18 +64,20 @@ const EditClients: ({ params }: EditClientsParameters) => Promise<React.ReactEle
     };
     return (
         <main>
-            <ClientForm
-                initialFields={initialFields}
-                initialFormErrors={initialFormErrors}
-                editMode
-                clientID={params.id}
-            />
+            {error ? (
+                <ErrorPage error={error} reset={() => router.refresh()} />
+            ) : (
+                initialFields && (
+                    <ClientForm
+                        initialFields={initialFields}
+                        initialFormErrors={initialFormErrors}
+                        editMode
+                        clientID={params.id}
+                    />
+                )
+            )}
         </main>
     );
-};
-
-export const metadata: Metadata = {
-    title: "Edit Clients",
 };
 
 export default EditClients;
