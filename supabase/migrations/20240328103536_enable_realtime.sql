f1049fe3b3cbdba3519219d1312d18ed70db556d
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-03-28 12:00:01 +0000

    NONE: Enable realtime on website_date

--- supabase/migrations/20240328103536_enable_realtime.sql
+++ supabase/migrations/20240328103536_enable_realtime.sql
@@ -1,1 +1,1 @@
-alter publication supabase_realtime add table clients, collection_centres, events, families, packing_slots, parcels;
+alter publication supabase_realtime add table clients, collection_centres, events, families, packing_slots, parcels, website_data;
