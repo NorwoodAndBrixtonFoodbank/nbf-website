923375c9a9f9aafb31f929497e1bf9faf6f9a529
minalui <mina.lui@softwire.com>
2024-04-02 09:39:03 +0100

    VFB-125: Add telephone regex for create user form

--- src/app/admin/createUser/UserDetailsCard.tsx
+++ src/app/admin/createUser/UserDetailsCard.tsx
@@ -1,13 +1,9 @@
 import FreeFormTextInput from "@/components/DataInput/FreeFormTextInput";
-import {
-    CardProps,
-    errorExists,
-    errorText,
-    onChangeText,
-} from "@/components/Form/formFunctions";
+import { CardProps, errorExists, errorText, onChangeText } from "@/components/Form/formFunctions";
 import GenericFormCard from "@/components/Form/GenericFormCard";
 import React from "react";
 
+const telephoneRegex = /^0\d{10}$/;
 const UserDetailsCard: React.FC<CardProps> = ({ fieldSetter, formErrors, errorSetter }) => {
     return (
         <GenericFormCard
@@ -18,21 +14,21 @@ const UserDetailsCard: React.FC<CardProps> = ({ fieldSetter, formErrors, errorSe
             <>
                 <FreeFormTextInput
                     label="First Name"
-                    error={errorExists(formErrors.email)}
-                    helperText={errorText(formErrors.email)}
+                    error={errorExists(formErrors.firstName)}
+                    helperText={errorText(formErrors.firstName)}
                     onChange={onChangeText(fieldSetter, errorSetter, "firstName", true)}
                 />
                 <FreeFormTextInput
                     label="Last Name"
-                    error={errorExists(formErrors.email)}
-                    helperText={errorText(formErrors.email)}
+                    error={errorExists(formErrors.lastName)}
+                    helperText={errorText(formErrors.lastName)}
                     onChange={onChangeText(fieldSetter, errorSetter, "lastName", true)}
                 />
                 <FreeFormTextInput
                     label="Telephone Number"
-                    error={errorExists(formErrors.email)}
-                    helperText={errorText(formErrors.email)}
-                    onChange={onChangeText(fieldSetter, errorSetter, "telephoneNumber", true)}
+                    error={errorExists(formErrors.telephoneNumber)}
+                    helperText={errorText(formErrors.telephoneNumber)}
+                    onChange={onChangeText(fieldSetter, errorSetter, "telephoneNumber", true, telephoneRegex)}
                 />
             </>
         </GenericFormCard>
