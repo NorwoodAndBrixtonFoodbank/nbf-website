f803ae3b844235d06aad4025c24b743abd25e4af
minalui <mina.lui@softwire.com>
2024-04-03 16:51:08 +0100

    VFB-125: Fix migration file

--- supabase/migrations/20240403101727_add_profiles_to_existing_users.sql
+++ supabase/migrations/20240403101727_add_profiles_to_existing_users.sql
@@ -13,14 +13,14 @@ DECLARE
     profile_role role;
 BEGIN
     INSERT INTO user_id_array (user_id, "role")
-    SELECT id, "role" FROM auth.users;
+    SELECT id, (raw_app_meta_data ->> 'role')::role FROM auth.users;
 
     SELECT COUNT(*) INTO total_count FROM user_id_array;
 
     WHILE index <= total_count LOOP
         SELECT user_id INTO profile_id FROM user_id_array WHERE array_index = index;
         SELECT "role" INTO profile_role FROM user_id_array WHERE array_index = index;
-        INSERT INTO profiles (user_id, "role") VALUES (profile_id, profile_role);
+        INSERT INTO profiles (primary_key, "role") VALUES (profile_id, profile_role);
         index := index + 1;
     END LOOP;
 END;
