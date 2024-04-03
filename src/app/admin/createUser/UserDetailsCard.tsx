05089104dee8593ba4678c71095e13a2267300d3
minalui <mina.lui@softwire.com>
2024-04-03 12:25:50 +0100

    VFB-125: Remove empty tag

--- src/app/admin/createUser/UserDetailsCard.tsx
+++ src/app/admin/createUser/UserDetailsCard.tsx
@@ -11,32 +11,30 @@ const UserDetailsCard: React.FC<CardProps> = ({ fieldSetter, formErrors, errorSe
             text="Please enter the relevant information for the new user."
             required
         >
-            <>
-                <FreeFormTextInput
-                    label="First Name"
-                    error={errorExists(formErrors.firstName)}
-                    helperText={errorText(formErrors.firstName)}
-                    onChange={onChangeText(fieldSetter, errorSetter, "firstName", true)}
-                />
-                <FreeFormTextInput
-                    label="Last Name"
-                    error={errorExists(formErrors.lastName)}
-                    helperText={errorText(formErrors.lastName)}
-                    onChange={onChangeText(fieldSetter, errorSetter, "lastName", true)}
-                />
-                <FreeFormTextInput
-                    label="Telephone Number"
-                    error={errorExists(formErrors.telephoneNumber)}
-                    helperText={errorText(formErrors.telephoneNumber)}
-                    onChange={onChangeText(
-                        fieldSetter,
-                        errorSetter,
-                        "telephoneNumber",
-                        true,
-                        telephoneRegex
-                    )}
-                />
-            </>
+            <FreeFormTextInput
+                label="First Name"
+                error={errorExists(formErrors.firstName)}
+                helperText={errorText(formErrors.firstName)}
+                onChange={onChangeText(fieldSetter, errorSetter, "firstName", true)}
+            />
+            <FreeFormTextInput
+                label="Last Name"
+                error={errorExists(formErrors.lastName)}
+                helperText={errorText(formErrors.lastName)}
+                onChange={onChangeText(fieldSetter, errorSetter, "lastName", true)}
+            />
+            <FreeFormTextInput
+                label="Telephone Number"
+                error={errorExists(formErrors.telephoneNumber)}
+                helperText={errorText(formErrors.telephoneNumber)}
+                onChange={onChangeText(
+                    fieldSetter,
+                    errorSetter,
+                    "telephoneNumber",
+                    true,
+                    telephoneRegex
+                )}
+            />
         </GenericFormCard>
     );
 };
