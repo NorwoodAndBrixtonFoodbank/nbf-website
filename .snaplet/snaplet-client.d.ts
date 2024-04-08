468b473f3edf8a9b0b4d54e6901dc55ad4e8c2b2
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-08 09:37:52 +0100

    snaplet gen

--- .snaplet/snaplet-client.d.ts
+++ .snaplet/snaplet-client.d.ts
@@ -27,38 +27,6 @@ type Override = {
       created?: string;
     };
   }
-  audit_log?: {
-    name?: string;
-    fields?: {
-      primary_key?: string;
-      user_id?: string;
-      action?: string;
-      client_id?: string;
-      collection_centre_id?: string;
-      event_id?: string;
-      family_member_id?: string;
-      list_id?: string;
-      list_hotel_id?: string;
-      packing_slot_id?: string;
-      parcel_id?: string;
-      status_order?: string;
-      website_data?: string;
-      content?: string;
-      wasSuccess?: string;
-      log_id?: string;
-      users?: string;
-      clients?: string;
-      collection_centres?: string;
-      events?: string;
-      families?: string;
-      lists?: string;
-      lists_hotel?: string;
-      packing_slots?: string;
-      parcels?: string;
-      status_order?: string;
-      website_data?: string;
-    };
-  }
   audit_log_entries?: {
     name?: string;
     fields?: {
@@ -85,6 +53,15 @@ type Override = {
       objects?: string;
     };
   }
+  channels?: {
+    name?: string;
+    fields?: {
+      id?: string;
+      name?: string;
+      inserted_at?: string;
+      updated_at?: string;
+    };
+  }
   clients?: {
     name?: string;
     fields?: {
@@ -106,7 +83,6 @@ type Override = {
       extra_information?: string;
       flagged_for_attention?: string;
       signposting_call_required?: string;
-      audit_log?: string;
       families?: string;
       parcels?: string;
     };
@@ -117,7 +93,6 @@ type Override = {
       name?: string;
       acronym?: string;
       primary_key?: string;
-      audit_log?: string;
       parcels?: string;
     };
   }
@@ -130,7 +105,6 @@ type Override = {
       parcel_id?: string;
       event_data?: string;
       parcels?: string;
-      audit_log?: string;
     };
   }
   families?: {
@@ -141,7 +115,6 @@ type Override = {
       gender?: string;
       age?: string;
       clients?: string;
-      audit_log?: string;
     };
   }
   flow_state?: {
@@ -255,7 +228,6 @@ type Override = {
       quantity_for_8?: string;
       quantity_for_9?: string;
       quantity_for_10?: string;
-      audit_log?: string;
     };
   }
   lists_hotel?: {
@@ -284,7 +256,6 @@ type Override = {
       quantity_for_8?: string;
       quantity_for_9?: string;
       quantity_for_10?: string;
-      audit_log?: string;
     };
   }
   mfa_amr_claims?: {
@@ -364,7 +335,6 @@ type Override = {
       name?: string;
       is_shown?: string;
       order?: string;
-      audit_log?: string;
       parcels?: string;
     };
   }
@@ -381,7 +351,6 @@ type Override = {
       clients?: string;
       collection_centres?: string;
       packing_slots?: string;
-      audit_log?: string;
       events?: string;
     };
   }
@@ -434,7 +403,6 @@ type Override = {
       for_email?: string;
       redirect_to?: string;
       from_ip_address?: string;
-      from_ip_address?: string;
       created_at?: string;
       updated_at?: string;
       flow_state_id?: string;
@@ -446,6 +414,13 @@ type Override = {
     name?: string;
     fields?: {
       version?: string;
+    };
+  }
+  realtime_schema_migrations?: {
+    name?: string;
+    fields?: {
+      version?: string;
+      inserted_at?: string;
     };
   }
   supabase_migrations_schema_migrations?: {
@@ -517,7 +492,18 @@ type Override = {
     fields?: {
       event_name?: string;
       workflow_order?: string;
-      audit_log?: string;
+    };
+  }
+  subscription?: {
+    name?: string;
+    fields?: {
+      id?: string;
+      subscription_id?: string;
+      entity?: string;
+      filters?: string;
+      claims?: string;
+      claims_role?: string;
+      created_at?: string;
     };
   }
   users?: {
@@ -560,7 +546,6 @@ type Override = {
       identities?: string;
       mfa_factors?: string;
       sessions?: string;
-      audit_log?: string;
       profiles?: string;
     };
   }
@@ -569,7 +554,6 @@ type Override = {
     fields?: {
       name?: string;
       value?: string;
-      audit_log?: string;
     };
   }}
 export type Alias = {
@@ -601,20 +585,6 @@ export interface Fingerprint {
     headers?: FingerprintJsonField;
     created?: FingerprintDateField;
   }
-  auditLogs?: {
-    content?: FingerprintJsonField;
-    user?: FingerprintRelationField;
-    clientByClientId?: FingerprintRelationField;
-    collectionCentreByCollectionCentreId?: FingerprintRelationField;
-    eventByEventId?: FingerprintRelationField;
-    familyByFamilyMemberId?: FingerprintRelationField;
-    listByListId?: FingerprintRelationField;
-    listsHotelByListHotelId?: FingerprintRelationField;
-    packingSlotByPackingSlotId?: FingerprintRelationField;
-    parcelByParcelId?: FingerprintRelationField;
-    statusOrderByStatusOrder?: FingerprintRelationField;
-    websiteDatumByWebsiteData?: FingerprintRelationField;
-  }
   auditLogEntries?: {
     payload?: FingerprintJsonField;
     createdAt?: FingerprintDateField;
@@ -625,24 +595,25 @@ export interface Fingerprint {
     fileSizeLimit?: FingerprintNumberField;
     objects?: FingerprintRelationField;
   }
+  channels?: {
+    id?: FingerprintNumberField;
+    insertedAt?: FingerprintDateField;
+    updatedAt?: FingerprintDateField;
+  }
   clients?: {
-    auditLogsByClientId?: FingerprintRelationField;
     families?: FingerprintRelationField;
     parcelsByClientId?: FingerprintRelationField;
   }
   collectionCentres?: {
-    auditLogsByCollectionCentreId?: FingerprintRelationField;
     parcelsByCollectionCentre?: FingerprintRelationField;
   }
   events?: {
     timestamp?: FingerprintDateField;
     parcelByParcelId?: FingerprintRelationField;
-    auditLogsByEventId?: FingerprintRelationField;
   }
   families?: {
     age?: FingerprintNumberField;
     family?: FingerprintRelationField;
-    auditLogsByFamilyMemberId?: FingerprintRelationField;
   }
   flowStates?: {
     createdAt?: FingerprintDateField;
@@ -681,11 +652,9 @@ export interface Fingerprint {
   }
   lists?: {
     rowOrder?: FingerprintNumberField;
-    auditLogsByListId?: FingerprintRelationField;
   }
   listsHotels?: {
     rowOrder?: FingerprintNumberField;
-    auditLogsByListHotelId?: FingerprintRelationField;
   }
   mfaAmrClaims?: {
     createdAt?: FingerprintDateField;
@@ -719,7 +688,6 @@ export interface Fingerprint {
   }
   packingSlots?: {
     order?: FingerprintNumberField;
-    auditLogsByPackingSlotId?: FingerprintRelationField;
     parcelsByPackingSlot?: FingerprintRelationField;
   }
   parcels?: {
@@ -728,7 +696,6 @@ export interface Fingerprint {
     clientByClientId?: FingerprintRelationField;
     collectionCentreByCollectionCentre?: FingerprintRelationField;
     packingSlotByPackingSlot?: FingerprintRelationField;
-    auditLogsByParcelId?: FingerprintRelationField;
     eventsByParcelId?: FingerprintRelationField;
   }
   profiles?: {
@@ -754,6 +721,10 @@ export interface Fingerprint {
   }
   authSchemaMigrations?: {
 
+  }
+  realtimeSchemaMigrations?: {
+    version?: FingerprintNumberField;
+    insertedAt?: FingerprintDateField;
   }
   supabaseMigrationsSchemaMigrations?: {
 
@@ -786,7 +757,11 @@ export interface Fingerprint {
   }
   statusOrders?: {
     workflowOrder?: FingerprintNumberField;
-    auditLogsByStatusOrder?: FingerprintRelationField;
+  }
+  subscriptions?: {
+    id?: FingerprintNumberField;
+    claims?: FingerprintJsonField;
+    createdAt?: FingerprintDateField;
   }
   users?: {
     emailConfirmedAt?: FingerprintDateField;
@@ -809,9 +784,8 @@ export interface Fingerprint {
     identities?: FingerprintRelationField;
     mfaFactors?: FingerprintRelationField;
     sessions?: FingerprintRelationField;
-    auditLogs?: FingerprintRelationField;
     profilesByPrimaryKey?: FingerprintRelationField;
   }
   websiteData?: {
-    auditLogsByWebsiteData?: FingerprintRelationField;
+
   }}
