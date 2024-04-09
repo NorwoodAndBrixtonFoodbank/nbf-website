0db7a7557a58e24ed62c250ca7cf97c3e674c7a6
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-09 11:46:35 +0100

    snaplet gen

--- .snaplet/snaplet-client.d.ts
+++ .snaplet/snaplet-client.d.ts
@@ -85,6 +85,15 @@ type Override = {
       objects?: string;
     };
   }
+  channels?: {
+    name?: string;
+    fields?: {
+      id?: string;
+      name?: string;
+      inserted_at?: string;
+      updated_at?: string;
+    };
+  }
   clients?: {
     name?: string;
     fields?: {
@@ -447,6 +456,13 @@ type Override = {
       version?: string;
     };
   }
+  realtime_schema_migrations?: {
+    name?: string;
+    fields?: {
+      version?: string;
+      inserted_at?: string;
+    };
+  }
   supabase_migrations_schema_migrations?: {
     name?: string;
     fields?: {
@@ -517,6 +533,18 @@ type Override = {
       event_name?: string;
       workflow_order?: string;
       audit_log?: string;
+    };
+  }
+  subscription?: {
+    name?: string;
+    fields?: {
+      id?: string;
+      subscription_id?: string;
+      entity?: string;
+      filters?: string;
+      claims?: string;
+      claims_role?: string;
+      created_at?: string;
     };
   }
   users?: {
@@ -623,6 +651,11 @@ export interface Fingerprint {
     updatedAt?: FingerprintDateField;
     fileSizeLimit?: FingerprintNumberField;
     objects?: FingerprintRelationField;
+  }
+  channels?: {
+    id?: FingerprintNumberField;
+    insertedAt?: FingerprintDateField;
+    updatedAt?: FingerprintDateField;
   }
   clients?: {
     auditLogsByClientId?: FingerprintRelationField;
@@ -754,6 +787,10 @@ export interface Fingerprint {
   authSchemaMigrations?: {
 
   }
+  realtimeSchemaMigrations?: {
+    version?: FingerprintNumberField;
+    insertedAt?: FingerprintDateField;
+  }
   supabaseMigrationsSchemaMigrations?: {
 
   }
@@ -786,6 +823,11 @@ export interface Fingerprint {
   statusOrders?: {
     workflowOrder?: FingerprintNumberField;
     auditLogsByStatusOrder?: FingerprintRelationField;
+  }
+  subscriptions?: {
+    id?: FingerprintNumberField;
+    claims?: FingerprintJsonField;
+    createdAt?: FingerprintDateField;
   }
   users?: {
     emailConfirmedAt?: FingerprintDateField;
