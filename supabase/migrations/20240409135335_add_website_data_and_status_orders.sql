de6a1b8c3995fab3dbe092b1e4105df472c48170
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-04-09 15:48:32 +0100

    VFB-188: Clean up

--- supabase/migrations/20240409135335_add_website_data_and_status_orders.sql
+++ supabase/migrations/20240409135335_add_website_data_and_status_orders.sql
@@ -1,8 +1,10 @@
 DELETE FROM public.website_data;
 
-INSERT INTO public.website_data (name,value) VALUES ('lists_text', 'Space is valuable! Please don''t leave boxes half empty - pack efficiently!
+INSERT INTO public.website_data (name,value) VALUES
+('lists_text', 'Space is valuable! Please don''t leave boxes half empty - pack efficiently!
 BOXES MUST BE PACKED FLAT SO THAT THEY CAN BE STACKED. Do not leave items sticking out of the top.
-We do have a selection of ''free from'' goods as well as vegan and halal products. If you''re uncertain about any additional dietary needs, please speak to one of the team.'), ('driver_overview_message', 'At the end of your shift, please call/text the Dispatch phone to let us know all deliveries have been completed or to report any issues.
+We do have a selection of ''free from'' goods as well as vegan and halal products. If you''re uncertain about any additional dietary needs, please speak to one of the team.'),
+('driver_overview_message', 'At the end of your shift, please call/text the Dispatch phone to let us know all deliveries have been completed or to report any issues.
 In an emergency, please ring Elizabeth or Rebekah.
 Dispatch- 07840 821 794 | Elizabeth- 07722 121 108 | Rebekah- 07366 574 794');
 
