95f5cbcf199c21c39c23d740dc09bd7694d01542
minalui <mina.lui@softwire.com>
2024-04-11 09:52:01 +0100

    VFB-167: Change to use profile.user_id instead of primary key

--- supabase/migrations/20240410094449_add_view_profiles_plus.sql
+++ supabase/migrations/20240410094449_add_view_profiles_plus.sql
@@ -7,7 +7,7 @@ create or replace view "public"."profiles_plus" as  SELECT users.id AS user_id,
     profiles.role,
     profiles.telephone_number
    FROM (auth.users
-     LEFT JOIN profiles ON ((users.id = profiles.primary_key)))
+     LEFT JOIN profiles ON ((users.id = profiles.user_id)))
   ORDER BY profiles.first_name;
 
 
