1316433bd52b1e5403af8b724e963a08f4739f3e
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-04-15 14:49:35 +0100

    #191: Add created_at to parcels_plus

--- supabase/migrations/20240415132809_add_creation_timestamp_to_parcels.sql
+++ supabase/migrations/20240415132809_add_creation_timestamp_to_parcels.sql
@@ -1,3 +1,33 @@
 alter table "public"."parcels" add column "created_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text);
 
-update "public"."parcels" set "created_at"='2023-12-31 12:00:00+00'
+update "public"."parcels" set "created_at"='2023-12-31 12:00:00+00';
+
+drop view if exists "public"."parcels_plus";
+
+create or replace view "public"."parcels_plus" as  SELECT parcels.primary_key AS parcel_id,
+    parcels.collection_datetime,
+    parcels.packing_date,
+    parcels.created_at,
+    packing_slots.name AS packing_slot_name,
+    packing_slots.order AS packing_slot_order,
+    parcels.voucher_number,
+    collection_centres.name AS collection_centre_name,
+    collection_centres.acronym AS collection_centre_acronym,
+    clients.primary_key AS client_id,
+    clients.full_name AS client_full_name,
+    clients.address_postcode AS client_address_postcode,
+    clients.flagged_for_attention AS client_flagged_for_attention,
+    clients.signposting_call_required AS client_signposting_call_required,
+    clients.phone_number AS client_phone_number,
+    family_count.family_count,
+    last_status.event_name AS last_status_event_name,
+    last_status.event_data AS last_status_event_data,
+    last_status."timestamp" AS last_status_timestamp,
+    last_status.workflow_order AS last_status_workflow_order
+   FROM (((((parcels
+     LEFT JOIN collection_centres ON ((parcels.collection_centre = collection_centres.primary_key)))
+     LEFT JOIN clients ON ((parcels.client_id = clients.primary_key)))
+     LEFT JOIN packing_slots ON ((parcels.packing_slot = packing_slots.primary_key)))
+     LEFT JOIN family_count ON ((family_count.family_id = clients.family_id)))
+     LEFT JOIN last_status ON ((last_status.parcel_id = parcels.primary_key)))
+  ORDER BY parcels.packing_date DESC;
