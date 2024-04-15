3e71854e9c2d6bf9f624e60733671d3e2fadf3db
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-04-15 14:32:46 +0100

    #191: Set creation time for existing parcels

--- supabase/migrations/20240415132809_add_creation_timestamp_to_parcels.sql
+++ supabase/migrations/20240415132809_add_creation_timestamp_to_parcels.sql
@@ -1,3 +1,3 @@
 alter table "public"."parcels" add column "created_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text);
 
-
+update "public"."parcels" set "created_at"='2023-12-31 12:00:00+00'
