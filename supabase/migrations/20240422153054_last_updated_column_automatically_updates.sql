10c47db5f827699333f622170c19fff666d22006
minalui <mina.lui@softwire.com>
2024-04-23 09:23:58 +0100

    VFB-81: Change update client function and automatically update timestamp

--- supabase/migrations/20240422153054_last_updated_column_automatically_updates.sql
+++
@@ -1,6 +0,0 @@
-create extension if not exists "moddatetime" with schema "extensions";
-
-
-CREATE TRIGGER handle_updated_at BEFORE UPDATE ON public.parcels FOR EACH ROW EXECUTE FUNCTION moddatetime('last_updated');
-
-
