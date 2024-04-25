7b1a6bbdac3a8940ecd4c4b645d0dc2f33422d3a
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-25 09:49:45 +0100

    Continuing work on migration file

--- supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
+++ supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
@@ -1,9 +1,11 @@
-alter table "public"."audit_log" add column "actor_profile_id" uuid not null;
+alter table "public"."audit_log" add column "actor_profile_id" uuid;
 
 alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(primary_key) not valid;
 
-update "public"."audit_log" set "actor_profile_id"="public"."profiles"."profile_id" from "public"."audit_log" inner join "public"."profiles" on "public"."audit_log"."user_id" = "public"."profiles"."user_id";
+update "public"."audit_log" as audit_log set "actor_profile_id" = profiles."primary_key" from "public"."profiles" as profiles where audit_log."user_id" = profiles."user_id";
+
+alter table "public"."audit_log" alter column "actor_profile_id" set not null;
 
 alter table "public"."audit_log" drop constraint "audit_log_profile_id_fkey";
 
-alter table "public"."audit_log" drop column "profile_id;
+alter table "public"."audit_log" drop column "user_id";
