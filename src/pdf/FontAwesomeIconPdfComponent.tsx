e56a70f21482a4003f32e673e88c25ab548309f7
minalui <mina.lui@softwire.com>
2024-04-25 17:19:26 +0100

    VFB-62: Improve readability

--- src/pdf/FontAwesomeIconPdfComponent.tsx
+++ src/pdf/FontAwesomeIconPdfComponent.tsx
@@ -7,18 +7,20 @@ interface FontAwesomeIconProps {
 }
 
 const FontAwesomeIconPdfComponent = ({ faIcon: { icon } }: FontAwesomeIconProps): ReactElement => {
-    const duotone = Array.isArray(icon[4]);
+    const isDuoTone = Array.isArray(icon[4]);
+    const width = icon[0];
+    const height = icon[1];
     const paths = Array.isArray(icon[4]) ? icon[4] : [icon[4]];
     const color = "black";
     return (
-        <Svg viewBox={`0 0 ${icon[0]} ${icon[1]}`} style={{ width: "20px" }}>
+        <Svg viewBox={`0 0 ${width} ${height}`} style={{ width: "20px" }}>
             {paths &&
                 paths.map((drawnPath, indexNumber) => (
                     <Path
                         d={drawnPath}
                         key="Font Awesome Icon"
                         fill={color}
-                        fillOpacity={duotone && indexNumber === 0 ? 0.4 : 1.0}
+                        fillOpacity={isDuoTone && indexNumber === 0 ? 0.4 : 1.0}
                     />
                 ))}
         </Svg>
