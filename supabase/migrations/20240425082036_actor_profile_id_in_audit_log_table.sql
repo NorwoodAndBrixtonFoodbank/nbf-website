fba7dad0b9cd964828a88659709258aa76b40ebc
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-25 10:21:36 +0100

    Maybe fix FK?

--- supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
+++ supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
@@ -1,11 +1,11 @@
 alter table "public"."audit_log" add column "actor_profile_id" uuid;
 
-alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("primary_key") ON DELETE CASCADE;
+alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey" FOREIGN KEY ("actor_profile_id") REFERENCES "public"."profiles"("primary_key") ON DELETE CASCADE;
 
-update "public"."audit_log" as audit_log set "actor_profile_id" = profiles."primary_key" from "public"."profiles" as profiles where audit_log."user_id" = profiles."user_id";
+-- update "public"."audit_log" as audit_log set "actor_profile_id" = profiles."primary_key" from "public"."profiles" as profiles where audit_log."user_id" = profiles."user_id";
 
-alter table "public"."audit_log" alter column "actor_profile_id" set not null;
+-- alter table "public"."audit_log" alter column "actor_profile_id" set not null;
 
-alter table "public"."audit_log" drop constraint "audit_log_profile_id_fkey";
+-- alter table "public"."audit_log" drop constraint "audit_log_profile_id_fkey";
 
-alter table "public"."audit_log" drop column "user_id";
+-- alter table "public"."audit_log" drop column "user_id";
