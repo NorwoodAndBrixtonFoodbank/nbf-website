a4af0c1a6af1675fd381760fdbbb8aad47d7ebd6
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-25 12:41:41 +0100

    On delete SET NULL and remove non null constraint

--- supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
+++ supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
@@ -4,8 +4,6 @@ alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey
 
 update "public"."audit_log" as audit_log set "actor_profile_id" = profiles."primary_key" from "public"."profiles" as profiles where audit_log."user_id" = profiles."user_id";
 
-alter table "public"."audit_log" alter column "actor_profile_id" set not null;
-
 alter table "public"."audit_log" drop constraint "audit_log_profile_id_fkey";
 
 alter table "public"."audit_log" drop column "user_id";
