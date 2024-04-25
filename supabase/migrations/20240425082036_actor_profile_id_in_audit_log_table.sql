d878f335ff46c80a7c3039ba470ae0a7fd861b59
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-25 12:30:30 +0100

    Set actor_profile_id null on delete

--- supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
+++ supabase/migrations/20240425082036_actor_profile_id_in_audit_log_table.sql
@@ -1,6 +1,6 @@
 alter table "public"."audit_log" add column "actor_profile_id" uuid;
 
-alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey" FOREIGN KEY ("actor_profile_id") REFERENCES "public"."profiles"("primary_key") ON DELETE CASCADE;
+alter table "public"."audit_log" add constraint "audit_log_actor_profile_id_fkey" FOREIGN KEY ("actor_profile_id") REFERENCES "public"."profiles"("primary_key") ON DELETE SET NULL;
 
 update "public"."audit_log" as audit_log set "actor_profile_id" = profiles."primary_key" from "public"."profiles" as profiles where audit_log."user_id" = profiles."user_id";
 
