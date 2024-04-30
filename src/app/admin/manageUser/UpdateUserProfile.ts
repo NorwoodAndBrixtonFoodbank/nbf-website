c7c86f3a81757030831375bc4091e01827972045
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-04-30 16:46:24 +0100

    Send audit log when editing a user

--- src/app/admin/manageUser/UpdateUserProfile.ts
+++ src/app/admin/manageUser/UpdateUserProfile.ts
@@ -1,6 +1,8 @@
 import supabase from "@/supabaseClient";
 import { PostgrestError } from "@supabase/supabase-js";
 import { UserRole } from "@/databaseUtils";
+import { logErrorReturnLogId } from "@/logger/logger";
+import { sendAuditLog } from "@/server/auditLog";
 
 interface UpdateUserProfile {
     userId: string;
@@ -13,14 +15,37 @@ interface UpdateUserProfile {
 export async function updateUserProfile(
     userDetails: UpdateUserProfile
 ): Promise<PostgrestError | null> {
-    const { error } = await supabase
+    const { data, error } = await supabase
         .from("profiles")
         .update({
             role: userDetails.role,
         })
         .eq("user_id", userDetails.userId)
-        .select()
+        .select("profile_id:primary_key")
         .single();
 
-    return error;
+    const auditLog = {
+        action: "edit a user",
+        content: {
+            role: userDetails.role,
+            firstName: userDetails.firstName,
+            lastName: userDetails.lastName,
+            phoneNumber: userDetails.phoneNumber,
+        },
+        userId: userDetails.userId,
+    };
+
+    if (error) {
+        const logId = await logErrorReturnLogId(
+            `Error with updating user profile: user id ${userDetails.userId}`,
+            {
+                error: error,
+            }
+        );
+        await sendAuditLog({ ...auditLog, wasSuccess: false, logId });
+        return error;
+    }
+
+    await sendAuditLog({ ...auditLog, wasSuccess: true, profileId: data.profile_id });
+    return null;
 }
