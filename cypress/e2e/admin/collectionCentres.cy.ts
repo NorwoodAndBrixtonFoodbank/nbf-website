2a0cad767ba56021bd1d4328851dad841e0c838e
minalui <mina.lui@softwire.com>
2024-04-30 15:54:23 +0100

    VFB-72: Add test to add a collection centre, use the last row

--- cypress/e2e/admin/collectionCentres.cy.ts
+++ cypress/e2e/admin/collectionCentres.cy.ts
@@ -1,18 +1,37 @@
+import { v4 as uuidv4 } from "uuid";
+
 describe("Edit a collection centre on admins page", () => {
     beforeEach(() => {
         cy.login();
         cy.visit("/admin");
     });
 
+    it("Adds a collection centre successfully"),
+        () => {
+            toggleCollectionCentreSection();
+            clickOnAddButton();
+            getNumberOfAriaRows().then((lastRow) => {
+                const newCollectionCentreName = generateRandomCollectionCentreText();
+                addCollectionCentreName(lastRow, newCollectionCentreName);
+                addCollectionCentreAcronym(lastRow, newCollectionCentreName);
+                clickOnSaveButton(lastRow);
+                cy.reload();
+                assertCollectionCentreName({
+                    rowIndex: lastRow,
+                    collectionCentreName: newCollectionCentreName,
+                });
+            });
+        };
+
     it("Hides a collection centre successfully", () => {
         toggleCollectionCentreSection();
 
-        assertCollectionCentreName({ rowIndex: 2, collectionCentreName: "Delivery" });
-
-        clickOnEditButton(2);
-        uncheckIsShownCheckbox(2);
-        clickOnSaveButton(2);
-        collectionCentreIsHidden(2);
+        getNumberOfAriaRows().then((lastRow) => {
+            clickOnEditButton(lastRow);
+            uncheckIsShownCheckbox(lastRow);
+            clickOnSaveButton(lastRow);
+            collectionCentreIsHidden(lastRow);
+        });
     });
 });
 
@@ -30,17 +49,58 @@ function assertCollectionCentreName({
 }): void {
     cy.contains(collectionCentresTableText, { matchCase: false })
         .parents(".MuiPaper-root")
-        .find(`[data-rowindex="${rowIndex}"]`) // eslint-disable-line quotes
+        .find(`[aria-rowindex="${rowIndex}"]`) // eslint-disable-line quotes
         .find('[data-field="name"]') // eslint-disable-line quotes
         .should(($slotName) => {
             expect($slotName).to.contain(collectionCentreName);
         });
 }
 
+function clickOnAddButton(): void {
+    cy.contains(collectionCentresTableText, { matchCase: false })
+        .parents(".MuiPaper-root")
+        .find('[data-testid="AddIcon"]') // eslint-disable-line quotes
+        .click();
+}
+
+function getNumberOfAriaRows(): Cypress.Chainable<number> {
+    return cy
+        .contains(collectionCentresTableText, { matchCase: false })
+        .parents(".MuiPaper-root")
+        .get('[aria-colcount="4"]') // eslint-disable-line quotes
+        .invoke("attr", "aria-rowcount")
+        .then((ariaRowCount) => {
+            const rowCount: number = parseInt(ariaRowCount as string, 10);
+            return cy.wrap(rowCount);
+        });
+}
+
+function addCollectionCentreName(rowIndex: number, text: string): void {
+    cy.contains(collectionCentresTableText, { matchCase: false })
+        .parents(".MuiPaper-root")
+        .find(`[aria-rowindex="${rowIndex}"]`)
+        .find('[data-field]="name"]') // eslint-disable-line quotes
+        .get('input[type="text"]') // eslint-disable-line quotes
+        .type(text);
+}
+
+function addCollectionCentreAcronym(rowIndex: number, text: string): void {
+    cy.contains(collectionCentresTableText, { matchCase: false })
+        .parents(".MuiPaper-root")
+        .find(`[aria-rowindex="${rowIndex}"]`)
+        .find('[data-field]="acronym"]') // eslint-disable-line quotes
+        .get('input[type="text"]') // eslint-disable-line quotes
+        .type(text);
+}
+
+function generateRandomCollectionCentreText(): string {
+    return uuidv4();
+}
+
 function clickOnEditButton(rowIndex: number): void {
     cy.contains(collectionCentresTableText, { matchCase: false })
         .parents(".MuiPaper-root")
-        .find(`[data-rowindex="${rowIndex}"]`)
+        .find(`[aria-rowindex="${rowIndex}"]`)
         .find('[data-testid="EditIcon"]') // eslint-disable-line quotes
         .click();
 }
@@ -48,7 +108,7 @@ function clickOnEditButton(rowIndex: number): void {
 function uncheckIsShownCheckbox(rowIndex: number): void {
     cy.contains(collectionCentresTableText, { matchCase: false })
         .parents(".MuiPaper-root")
-        .find(`[data-rowindex="${rowIndex}"]`)
+        .find(`[aria-rowindex="${rowIndex}"]`)
         .find('[type="checkbox"]') // eslint-disable-line quotes
         .uncheck();
 }
@@ -56,7 +116,7 @@ function uncheckIsShownCheckbox(rowIndex: number): void {
 function clickOnSaveButton(rowIndex: number): void {
     cy.contains(collectionCentresTableText, { matchCase: false })
         .parents(".MuiPaper-root")
-        .find(`[data-rowindex="${rowIndex}"]`)
+        .find(`[aria-rowindex="${rowIndex}"]`)
         .find('[data-testid="SaveIcon"]') // eslint-disable-line quotes
         .click();
 }
@@ -64,7 +124,7 @@ function clickOnSaveButton(rowIndex: number): void {
 function collectionCentreIsHidden(rowIndex: number): void {
     cy.contains(collectionCentresTableText, { matchCase: false })
         .parents(".MuiPaper-root")
-        .find(`[data-rowindex="${rowIndex}"]`)
+        .find(`[aria-rowindex="${rowIndex}"]`)
         .find('[data-testid="CloseIcon"]') // eslint-disable-line quotes
         .should("exist");
 }
