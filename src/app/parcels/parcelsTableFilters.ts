f38d6cd24a91cd4c6a2e7519b8080a0fdd462799
Kyle Liew <kyle.liew@softwire.com>
2024-05-09 14:49:15 +0100

    changing to doublequote

--- src/app/parcels/parcelsTableFilters.ts
+++ src/app/parcels/parcelsTableFilters.ts
@@ -77,8 +77,8 @@ export const voucherSearch = (
     query: PostgrestFilterBuilder<Database["public"], any, any>,
     state: string
 ): PostgrestFilterBuilder<Database["public"], any, any> => {
-    if (state === '?') {
-        return query.ilike("voucher_number", ``);
+    if (state === "?") {
+        return query.ilike("voucher_number", "");
     }
     return query.ilike("voucher_number", `%${state}%`);
 };
