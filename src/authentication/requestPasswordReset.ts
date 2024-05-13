e387884b94393fea241f582eb8792d465617c1d1
Max Johnson <max.johnson@softwire.com>
2024-05-13 11:18:33 +0100

    #193: Add help text when HTTP 429 is returned

--- src/authentication/requestPasswordReset.ts
+++ src/authentication/requestPasswordReset.ts
@@ -1,8 +1,18 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { logErrorReturnLogId } from "@/logger/logger";
+import { AuthError } from "@supabase/gotrue-js";
 
 interface RequestPasswordResetResponse {
     errorMessage: string | null;
+}
+
+function formatErrorMessage(error: AuthError): string {
+    let errorMessage = error.message;
+    // HTTP 429 is "too many requests". Usually means email rate limit has been exceeded.
+    if (error.status === 429) {
+        errorMessage += ".\nPlease contact admin for help.\nhelp@foodbankapp.org";
+    }
+    return errorMessage;
 }
 
 export async function requestPasswordReset({
@@ -18,7 +28,7 @@ export async function requestPasswordReset({
 
     if (error) {
         void logErrorReturnLogId("Password reset request failed", { error });
-        return { errorMessage: error.message };
+        return { errorMessage: formatErrorMessage(error) };
     }
 
     return { errorMessage: null };
