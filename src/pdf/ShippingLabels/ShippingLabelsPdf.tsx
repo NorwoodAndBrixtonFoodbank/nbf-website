10c41671048ad68f2fddd1bbcc120c080d33609c
Kyle Liew <kyle.liew@softwire.com>
2024-05-14 09:56:54 +0100

    VFB-130: Made all required adjustments - hard coded size of page

--- src/pdf/ShippingLabels/ShippingLabelsPdf.tsx
+++ src/pdf/ShippingLabels/ShippingLabelsPdf.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import { nullPostcodeDisplay } from "@/common/format";
-import { faBuildingCircleArrowRight, faTruck } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarXmark, faShoePrints, faTruck } from "@fortawesome/free-solid-svg-icons";
 import FontAwesomeIconPdfComponent from "@/pdf/FontAwesomeIconPdfComponent";
+import { AlignHorizontalLeft } from "@mui/icons-material";
 
 export interface ShippingLabelData {
     label_quantity: number;
@@ -23,17 +24,18 @@ export interface ShippingLabelData {
 }
 
 const mmToPixels = (mm: number): number => {
-    const pixelsPerMmAt72Dpi = 72 / 25.4;
-    return mm * pixelsPerMmAt72Dpi;
+    const inches = mm / 2.54
+    const pixelsPerCm = 21.285;
+    return mm * pixelsPerCm;
 };
 
-const labelSizePixels = { width: mmToPixels(150), height: mmToPixels(62) };
+const labelSizePixels = { width: mmToPixels(20), height: mmToPixels(6.2) };
 
 const styles = StyleSheet.create({
     page: {
         padding: "0.2cm",
         fontFamily: "Helvetica",
-        fontSize: "11pt",
+        fontSize: "10pt",
         lineHeight: 1.25,
     },
     cardWrapper: {
@@ -44,7 +46,7 @@ const styles = StyleSheet.create({
         padding: "0.2cm",
     },
     topRow: {
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         flexBasis: "20%",
         display: "flex",
         flexDirection: "row",
@@ -52,22 +54,22 @@ const styles = StyleSheet.create({
     },
     middleRow: { flex: 1, display: "flex", flexDirection: "row" },
     bottomRow: {
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         flexBasis: "30%",
         display: "flex",
         flexDirection: "row",
     },
-    leftCol: { justifyContent: "flex-start", flexBasis: "36%" },
-    middleCol: { flex: 1 },
-    rightCol: { justifyContent: "space-evenly", flexBasis: "25%" },
-    bottomAlign: { marginTop: "auto" },
+    leftCol: { flexBasis: "32%", textAlign: "left" },
+    middleCol: { flex: 1, textAlign: "left" },
+    rightCol: { flexBasis: "28%", textAlign: "right"},
+    bottomAlign: { marginTop: "13px"},
     headingText: { fontFamily: "Helvetica-Bold", textTransform: "uppercase" },
     largeText: {
-        fontSize: "29pt",
+        fontSize: "20pt",
         lineHeight: 1,
     },
     mediumText: {
-        fontSize: "19pt",
+        fontSize: "15pt",
         lineHeight: 1,
     },
 });
@@ -81,20 +83,22 @@ interface LabelCardProps {
 const SingleLabelCard: React.FC<LabelCardProps> = ({ data, index, quantity }) => {
     return (
         <Page size={labelSizePixels} style={styles.page}>
-            <View style={styles.cardWrapper} wrap={false}>
-                <View style={styles.topRow}>
-                    <Text style={styles.leftCol}>
-                        <Text style={styles.headingText}>Name:</Text>
-                        <Text> {data.full_name}</Text>
-                    </Text>
-                    <Text style={styles.middleCol}>
-                        <Text style={styles.headingText}>Contact:</Text>
-                        <Text> {data.phone_number}</Text>
-                    </Text>
-                    <Text style={styles.rightCol}>
-                        <Text style={styles.headingText}>Packed:</Text>
-                        <Text>{data.packing_date} {data.packing_slot}</Text>
-                    </Text>
+            <View style={styles.cardWrapper} wrap={true}>
+                <View style={[styles.topRow]}>
+                    <View style={[styles.leftCol, {flexDirection: "row"}]}>
+                        <Text style={styles.headingText}>Name: </Text>
+                        <Text>{data.full_name}</Text>
+                    </View>
+                    <View style={[styles.middleCol, {flexDirection: "row"}]}>
+                        <Text style={styles.headingText}>Contact: </Text>
+                        <Text>{data.phone_number}</Text>
+                    </View>
+                    <View style={[styles.rightCol]}>
+                        <View style={{flexDirection: "row", justifyContent: "flex-end"}}>
+                            <Text style={[styles.headingText]}>Packed:{" "}</Text>
+                            <Text>{data.packing_date}</Text>
+                        </View>    
+                    </View>
                 </View>
                 <View style={styles.middleRow}>
                     <View style={styles.leftCol}>
@@ -120,34 +124,44 @@ const SingleLabelCard: React.FC<LabelCardProps> = ({ data, index, quantity }) =>
                         )}
                     </View>
                     <View style={styles.middleCol}>
-                        <Text style={styles.headingText}>Delivery Instructions:</Text>
+                        <Text style={styles.headingText}>Delivery Instructions: </Text>
                         <Text>{data.delivery_instructions}</Text>
                     </View>
                 </View>
-                <View style={styles.bottomRow}>
-                    <View style={[styles.leftCol, styles.bottomAlign]}>
+                <View style={[styles.bottomRow]}>
+                    <View style={[styles.leftCol, {marginTop: "10px"}]}>
                         <Text style={styles.largeText}>
                             {data.address_postcode ?? nullPostcodeDisplay}
                         </Text>
                     </View>
-                    <View style={[styles.middleCol, styles.bottomAlign, { flexDirection: "row" }]}>
-                        <Text style={styles.mediumText}>
-                            {data.packing_slot} |
-                            {data.collection_centre === "DLVR"
-                                ? "Delivery "
-                                : data.collection_centre + " "}
-                        </Text>
-                        <FontAwesomeIconPdfComponent
-                            faIcon={
-                                data.collection_centre === "DLVR"
-                                    ? faTruck
-                                    : faBuildingCircleArrowRight
-                            }
-                        ></FontAwesomeIconPdfComponent>
+                    <View style={[styles.middleCol, { flexDirection: "row"}]}>
+                        <View style = {[styles.bottomAlign]}>
+                            <Text style={styles.mediumText}>
+                                {data.packing_slot}{" "} 
+                            </Text>
+                        </View>
+                        <View style = {{marginTop: "auto"}}>
+                            <Text style={{fontWeight: "bold", fontSize: "20pt"}}>|</Text>
+                        </View>
+                        <View style = {[styles.bottomAlign, { flexDirection: "row" }]}>
+                            <Text style={styles.mediumText}>
+                                {" "}
+                                {data.collection_centre === "DLVR"
+                                    ? "Delivery "
+                                    : data.collection_centre + " "}
+                            </Text>
+                            <FontAwesomeIconPdfComponent
+                                faIcon={
+                                    data.collection_centre === "DLVR"
+                                        ? faTruck
+                                        : faShoePrints
+                                }
+                            ></FontAwesomeIconPdfComponent>
+                        </View>
                     </View>
                     <View style={[styles.rightCol, styles.bottomAlign]}>
                         <Text style={styles.mediumText}>
-                            Parcel {index + 1} of {quantity}
+                            {index + 1} of {quantity}
                         </Text>
                     </View>
                 </View>
