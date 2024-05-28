8020bb2e29506c8e5bc10cf89d27fc9103433fac
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-05-28 11:57:08 +0100

    Further filter fixes

--- src/app/parcels/parcelsTable/filters.ts
+++ src/app/parcels/parcelsTable/filters.ts
@@ -21,11 +21,15 @@ import { parcelTableHeaderKeysAndLabels } from "./headers";
 import { DbParcelRow } from "@/databaseUtils";
 import { fullNameSearch, phoneSearch, postcodeSearch } from "@/common/databaseFilters";
 
-const parcelsFullNameSearch: ParcelsFilterMethod<string> =
-    fullNameSearch<DbParcelRow>("client_full_name");
+const parcelsFullNameSearch: ParcelsFilterMethod<string> = fullNameSearch<DbParcelRow>(
+    "client_full_name",
+    "client_is_active"
+);
 
-const parcelsPostcodeSearch: ParcelsFilterMethod<string> =
-    postcodeSearch<DbParcelRow>("client_address_postcode");
+const parcelsPostcodeSearch: ParcelsFilterMethod<string> = postcodeSearch<DbParcelRow>(
+    "client_address_postcode",
+    "client_is_active"
+);
 
 const familySearch: ParcelsFilterMethod<string> = (query, state) => {
     if (state === "") {
@@ -53,8 +57,10 @@ const familySearch: ParcelsFilterMethod<string> = (query, state) => {
     return query.eq("family_count", Number(state));
 };
 
-const parcelsPhoneSearch: ParcelsFilterMethod<string> =
-    phoneSearch<DbParcelRow>("client_phone_number");
+const parcelsPhoneSearch: ParcelsFilterMethod<string> = phoneSearch<DbParcelRow>(
+    "client_phone_number",
+    "client_is_active"
+);
 
 const voucherSearch: ParcelsFilterMethod<string> = (query, state) => {
     if (state === "?") {
--- src/common/databaseFilters.ts
+++ src/common/databaseFilters.ts
@@ -4,7 +4,7 @@ import { DbClientRow, DbParcelRow } from "@/databaseUtils";
 import { parcelsPageDeletedClientDisplayName } from "@/app/parcels/parcelsTable/format";
 
 export const fullNameSearch = <DbData extends DbClientRow | DbParcelRow>(
-    columnLabel: Extract<keyof DbData, "full_name" | "client_full_name">,
+    fullNameColumnLabel: Extract<keyof DbData, "full_name" | "client_full_name">,
     clientIsActiveColumnLabel: Extract<keyof DbData, "is_active" | "client_is_active">
 ): ServerSideFilterMethod<DbData, string> => {
     return (query, state) => {
@@ -13,15 +13,15 @@ export const fullNameSearch = <DbData extends DbClientRow | DbParcelRow>(
         }
         if (parcelsPageDeletedClientDisplayName.toLowerCase().includes(state.toLowerCase())) {
             return query.or(
-                `${columnLabel}.ilike.%${state}%, ${clientIsActiveColumnLabel}.eq.false`
+                `${fullNameColumnLabel}.ilike.%${state}%, ${clientIsActiveColumnLabel}.eq.false`
             );
         }
-        return query.ilike(`${columnLabel}`, `%${state}%`);
+        return query.ilike(`${fullNameColumnLabel}`, `%${state}%`);
     };
 };
 
 export const postcodeSearch = <DbData extends DbClientRow | DbParcelRow>(
-    columnLabel: Extract<keyof DbData, "address_postcode" | "client_address_postcode">,
+    postcodeColumnLabel: Extract<keyof DbData, "address_postcode" | "client_address_postcode">,
     clientIsActiveColumnLabel: Extract<keyof DbData, "is_active" | "client_is_active">
 ): ServerSideFilterMethod<DbData, string> => {
     return (query, state) => {
@@ -29,19 +29,19 @@ export const postcodeSearch = <DbData extends DbClientRow | DbParcelRow>(
             return query;
         }
         if (state === "-") {
-            return query.is(`${columnLabel}`, null);
+            return query.is(clientIsActiveColumnLabel, false);
         }
         if (displayPostcodeForHomelessClient.toLowerCase().includes(state.toLowerCase())) {
-            return query.or(
-                `${columnLabel}.ilike.%${state}%, ${clientIsActiveColumnLabel}.eq.false`
-            );
+            return query
+                .or(`${postcodeColumnLabel}.ilike.%${state}%, ${postcodeColumnLabel}.is.null`)
+                .neq(clientIsActiveColumnLabel, false);
         }
-        return query.ilike(`${columnLabel}`, `%${state}%`);
+        return query.ilike(`${postcodeColumnLabel}`, `%${state}%`);
     };
 };
 
 export const phoneSearch = <DbData extends DbClientRow | DbParcelRow>(
-    columnLabel: Extract<keyof DbData, "phone_number" | "client_phone_number">,
+    phoneColumnLabel: Extract<keyof DbData, "phone_number" | "client_phone_number">,
     clientIsActiveColumnLabel: Extract<keyof DbData, "is_active" | "client_is_active">
 ): ServerSideFilterMethod<DbData, string> => {
     return (query, state) => {
@@ -50,9 +50,9 @@ export const phoneSearch = <DbData extends DbClientRow | DbParcelRow>(
         }
         if ("-".includes(state.toLowerCase())) {
             return query.or(
-                `${columnLabel}.ilike.%${state}%, ${clientIsActiveColumnLabel}.eq.false`
+                `${phoneColumnLabel}.ilike.%${state}%, ${clientIsActiveColumnLabel}.eq.false`
             );
         }
-        return query.ilike(`${columnLabel}`, `%${state}%`);
+        return query.ilike(`${phoneColumnLabel}`, `%${state}%`);
     };
 };
