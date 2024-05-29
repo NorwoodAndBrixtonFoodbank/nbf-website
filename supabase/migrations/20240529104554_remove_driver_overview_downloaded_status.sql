9ab8b510590fcfb526d27be1911e878022798b42
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-05-29 11:51:03 +0100

    update migration

--- supabase/migrations/20240529104554_remove_driver_overview_downloaded_status.sql
+++ supabase/migrations/20240529104554_remove_driver_overview_downloaded_status.sql
@@ -1,3 +1,11 @@
 update public.events set new_parcel_status = 'Out for Delivery', event_data = 'with John' where new_parcel_status = 'Driver Overview Downloaded';
 
 delete from public.status_order where event_name = 'Driver Overview Downloaded';
+
+update public.status_order set workflow_order = 12 where event_name = 'Map Generated';
+update public.status_order set workflow_order = 13 where event_name = 'Out for Delivery';
+update public.status_order set workflow_order = 14 where event_name = 'Delivered';
+update public.status_order set workflow_order = 15 where event_name = 'Delivery Failed';
+update public.status_order set workflow_order = 16 where event_name = 'Delivery Cancelled';
+update public.status_order set workflow_order = 17 where event_name = 'Fulfilled with Trussell Trust';
+update public.status_order set workflow_order = 18 where event_name = 'Parcel Deleted';
