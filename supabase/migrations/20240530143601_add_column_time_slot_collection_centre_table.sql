f28730884628610e82523e99a90b8c7f148b3c61
minalui <mina.lui@softwire.com>
2024-05-30 16:38:05 +0100

    #233: Remove default values for time_slot column

--- supabase/migrations/20240530143601_add_column_time_slot_collection_centre_table.sql
+++ supabase/migrations/20240530143601_add_column_time_slot_collection_centre_table.sql
@@ -1,23 +1,3 @@
 create type "public"."collection_timeslot_type" as ("time" time without time zone, "is_active" boolean);
 
-alter table "public"."collection_centres" add column "time_slots" collection_timeslot_type[] default array[
-     ROW('10:00:00', TRUE)::collection_timeslot_type,
-     ROW('10:15:00', TRUE)::collection_timeslot_type,
-     ROW('10:30:00', TRUE)::collection_timeslot_type,
-     ROW('10:45:00', TRUE)::collection_timeslot_type,
-     ROW('11:00:00', TRUE)::collection_timeslot_type,
-     ROW('11:15:00', TRUE)::collection_timeslot_type,
-     ROW('11:30:00', TRUE)::collection_timeslot_type,
-     ROW('11:45:00', TRUE)::collection_timeslot_type,
-     ROW('12:00:00', TRUE)::collection_timeslot_type,
-     ROW('12:15:00', TRUE)::collection_timeslot_type,
-     ROW('12:30:00', TRUE)::collection_timeslot_type,
-     ROW('12:45:00', TRUE)::collection_timeslot_type,
-     ROW('13:00:00', TRUE)::collection_timeslot_type,
-     ROW('13:15:00', TRUE)::collection_timeslot_type,
-     ROW('13:30:00', TRUE)::collection_timeslot_type,
-     ROW('13:45:00', TRUE)::collection_timeslot_type,
-     ROW('14:00:00', TRUE)::collection_timeslot_type
- ];
-
-alter table "public"."collection_centres" alter column "time_slots" set not null;
+alter table "public"."collection_centres" add column "time_slots" collection_timeslot_type[];
