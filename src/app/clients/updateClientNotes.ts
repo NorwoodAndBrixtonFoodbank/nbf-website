277ffd6a7d0c9cec4e86630e5740406e53519bb7
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-05-31 14:09:34 +0100

    Audit log client notes updates

--- src/app/clients/updateClientNotes.ts
+++ src/app/clients/updateClientNotes.ts
@@ -1,4 +1,5 @@
 import { logErrorReturnLogId } from "@/logger/logger";
+import { sendAuditLog } from "@/server/auditLog";
 import supabase from "@/supabaseClient";
 
 type UpdateClientNotesResponse =
@@ -9,14 +10,20 @@ export const updateClientNotes = async (
     clientId: string,
     notes: string | null
 ): Promise<UpdateClientNotesResponse> => {
+    const baseAuditLogProps = {
+        action: "update client notes",
+        clientId,
+        content: { notes: notes, clientId },
+    };
     const { error } = await supabase
         .from("clients")
         .update({ notes: notes })
         .eq("primary_key", clientId);
     if (error) {
         const logId = await logErrorReturnLogId("update client notes failed", { error });
+        await sendAuditLog({ ...baseAuditLogProps, wasSuccess: false, logId });
         return { error: { type: "updateNotesFailed", logId } };
     }
-    //aduit log
+    await sendAuditLog({ ...baseAuditLogProps, wasSuccess: true });
     return { error: null };
 };
