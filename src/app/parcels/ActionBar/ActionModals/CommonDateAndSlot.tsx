9cc1d251ba1ce2c6fc2e4eb38e2eaf5192e3de40
Kyle Liew <kyle.liew@softwire.com>
2024-05-31 17:31:15 +0100

    #302: minor workflow changes for packing date/slot error handling

--- src/app/parcels/ActionBar/ActionModals/CommonDateAndSlot.tsx
+++ src/app/parcels/ActionBar/ActionModals/CommonDateAndSlot.tsx
@@ -77,62 +77,64 @@ export const packingDateOrSlotUpdate = async (
     }
 
     const { data: parcelData, error: fetchError } = await fetchParcel(parcel.parcelId, supabase);
-    if (fetchError) {
-        const logId = await logErrorReturnLogId("Error with fetching parcel data", fetchError);
-        await sendAuditLog({
-            action: action,
-            content: {
-                parcelDetails: {
-                    client_id: parcel.clientId,
-                    packing_date: parcel.packingDate?.toString(),
-                    packing_slot: parcel.packingSlot,
-                    voucher_number: parcel.voucherNumber,
-                    collection_centre: parcel.deliveryCollection.collectionCentreName,
-                    collection_datetime: parcel.collectionDatetime?.toString(),
+    if (updateResponse.error || updateResponse.count === 0) {
+        if (fetchError) {
+            const logId = await logErrorReturnLogId("Error with fetching parcel data", fetchError);
+            await sendAuditLog({
+                action: action,
+                content: {
+                    parcelDetails: {
+                        client_id: parcel.clientId,
+                        packing_date: parcel.packingDate?.toString(),
+                        packing_slot: parcel.packingSlot,
+                        voucher_number: parcel.voucherNumber,
+                        collection_centre: parcel.deliveryCollection.collectionCentreName,
+                        collection_datetime: parcel.collectionDatetime?.toString(),
+                    },
                 },
-            },
-            wasSuccess: false,
-            logId,
-        });
-        return { parcelId: parcel.parcelId, error: fetchError };
-    }
+                wasSuccess: false,
+                logId,
+            });
+            return { parcelId: parcel.parcelId, error: fetchError };
+        }
 
-    const parcelRecord = {
-        client_id: parcelData.client_id,
-        packing_date: parcelData.packing_date,
-        packing_slot: parcelData.packing_slot?.primary_key,
-        voucher_number: parcelData.voucher_number,
-        collection_centre: parcelData.collection_centre?.primary_key,
-        collection_datetime: parcelData.collection_datetime,
-        last_updated: parcelData.last_updated,
-    };
-
-    const auditLog = {
-        action: action,
-        content: { parcelDetails: parcelRecord, count: updateResponse.count },
-        clientId: parcel.clientId,
-        parcelId: parcel.parcelId,
-    } as const satisfies Partial<AuditLog>;
-
-    if (updateResponse.error) {
-        const logId = await logErrorReturnLogId(
-            "Error with update: parcel data",
-            updateResponse.error
-        );
-        await sendAuditLog({ ...auditLog, wasSuccess: false, logId });
-        return {
-            parcelId: null,
-            error: { type: "failedToUpdateParcel", logId } as UpdateParcelError,
+        const parcelRecord = {
+            client_id: parcelData.client_id,
+            packing_date: parcelData.packing_date,
+            packing_slot: parcelData.packing_slot?.primary_key,
+            voucher_number: parcelData.voucher_number,
+            collection_centre: parcelData.collection_centre?.primary_key,
+            collection_datetime: parcelData.collection_datetime,
+            last_updated: parcelData.last_updated,
         };
-    }
 
-    if (updateResponse.count === 0) {
-        const logId = await logWarningReturnLogId("Concurrent editing of parcel");
-        await sendAuditLog({ ...auditLog, wasSuccess: false, logId });
-        return {
-            parcelId: null,
-            error: { type: "concurrentUpdateConflict", logId } as UpdateParcelError,
-        };
+        const auditLog = {
+            action: action,
+            content: { parcelDetails: parcelRecord, count: updateResponse.count },
+            clientId: parcel.clientId,
+            parcelId: parcel.parcelId,
+        } as const satisfies Partial<AuditLog>;
+
+        if (updateResponse.error) {
+            const logId = await logErrorReturnLogId(
+                "Error with update: parcel data",
+                updateResponse.error
+            );
+            await sendAuditLog({ ...auditLog, wasSuccess: false, logId });
+            return {
+                parcelId: null,
+                error: { type: "failedToUpdateParcel", logId } as UpdateParcelError,
+            };
+        }
+
+        if (updateResponse.count === 0) {
+            const logId = await logWarningReturnLogId("Concurrent editing of parcel");
+            await sendAuditLog({ ...auditLog, wasSuccess: false, logId });
+            return {
+                parcelId: null,
+                error: { type: "concurrentUpdateConflict", logId } as UpdateParcelError,
+            };
+        }
     }
 
     return { parcelId: parcel.parcelId, error: null };
