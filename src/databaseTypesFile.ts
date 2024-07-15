226a3e2d9ba04a3aff31a9395212f1f7cf8e5557
Ebinezer Rajaram <ebinezer.rajaram@softwire.com>
2024-07-15 11:16:39 +0100

    Add ListType enum to databaseTypesFile.ts

--- src/databaseTypesFile.ts
+++ src/databaseTypesFile.ts
@@ -1049,6 +1049,7 @@ export type Database = {
     }
     Enums: {
       gender: "male" | "female" | "other"
+      list_type: "regular" | "hotel"
       role: "volunteer" | "admin" | "manager" | "staff"
     }
     CompositeTypes: {
