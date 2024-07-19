628f33108672b659a9c62f52120c15b4ff84c6d8
Rebecca Jesson <Rebecca.Jesson@softwire.com>
2024-07-19 10:17:27 +0100

    VFB-262 remove unnecessary route config

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 import { ReactElement } from "react";
 import InfoPage, { getTopWikiRow } from "@/app/info/InfoPage";
 import Title from "@/components/Title/Title";
-export const dynamic = "force-dynamic";
 
 const Info = async (): Promise<ReactElement> => {
     const props = await getTopWikiRow();
