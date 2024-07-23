ba32d712d61d3b4bf3a06b4e21ebf5807f69ad14
Rebecca Jesson <Rebecca.Jesson@softwire.com>
2024-07-23 15:25:40 +0100

    VFB-280 fix typing in clientform

--- src/app/clients/form/ClientForm.tsx
+++ src/app/clients/form/ClientForm.tsx
@@ -40,6 +40,7 @@ import ClientNotesCard from "@/app/clients/form/formSections/ClientNotes";
 import { getCurrentYear } from "@/common/date";
 import { youngestAdultBirthYear } from "@/app/clients/form/birthYearDropdown";
 import ListTypeCard from "./formSections/ListTypeCard";
+import { ListName } from "@/app/lists/ListDataview";
 
 interface Props {
     initialFields: ClientFields;
@@ -61,7 +62,7 @@ export interface ClientFields extends Fields {
     numberOfAdults: number;
     children: Person[];
     numberOfChildren: number;
-    listType: string;
+    listType: ListName;
     dietaryRequirements: BooleanGroup;
     feminineProducts: BooleanGroup;
     babyProducts: boolean | null;
