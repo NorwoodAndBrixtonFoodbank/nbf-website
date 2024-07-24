852bff2c08c2391eddd8a7e49cff80c76db5f63c
Ebinezer Rajaram <ebinezer.rajaram@softwire.com>
2024-07-24 11:44:57 +0100

    VFB-292 WIP jest mock experimenting

--- src/app/parcels/ActionBar/ActionAndStatusBar.test.tsx
+++ src/app/parcels/ActionBar/ActionAndStatusBar.test.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { expect, it } from "@jest/globals";
 import "@testing-library/jest-dom/jest-globals";
-import ActionAndStatusBar, {
-    ActionAndStatusBarProps,
-} from "@/app/parcels/ActionBar/ActionAndStatusBar";
+import ActionAndStatusBar, { ActionAndStatusBarProps} from "@/app/parcels/ActionBar/ActionAndStatusBar";
 import { ParcelsTableRow } from "../parcelsTable/types";
 import StyleManager from "@/app/themes";
 import Localization from "@/app/Localization";
@@ -21,7 +19,6 @@ const mockProps: ActionAndStatusBarProps = {
 };
 
 jest.mock("@/supabaseClient", () => {
-    console.log("Mocking Supabase client");
     return { default: jest.fn() };
 });
 
@@ -31,31 +28,46 @@ jest.mock("@/logger/logger", () => ({
 }));
 
 jest.mock("@/app/parcels/ActionBar/Statuses", () => {
-    console.log("Mocking Statuses component"); // Add logging statement
+  console.log("Mocking Statuses component"); 
+  return jest.fn().mockImplementation(() => (
+      <div>
+          <button data-testid="#status-button">Statuses</button>
+          <div data-testid="#status-menu">
+              <button>Parcel Denied</button>
+              {/* Add more status options as needed */}
+          </div>
+      </div>
+  ));
+});
+
+jest.mock("@/app/parcels/ActionBar/Actions", () => {
+    console.log("Mocking Statuses component"); 
     return jest.fn().mockImplementation(() => (
         <div>
-            <button data-testid="#status-button">Statuses</button>
+            <button data-testid="#status-button">Actions</button>
             <div data-testid="#status-menu">
-                <button>Parcel Denied</button>
+                <button>Download Shopping List</button>
                 {/* Add more status options as needed */}
             </div>
         </div>
     ));
-});
+  });
+  
 
-Object.defineProperty(window, "matchMedia", {
+
+Object.defineProperty(window, 'matchMedia', {
     writable: true,
-    value: jest.fn().mockImplementation((query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), 
+      removeListener: jest.fn(), 
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
     })),
-});
+  });
 
 const MockActionBar: React.FC<ActionAndStatusBarProps> = ({
     fetchSelectedParcels: fetchSelectedParcels,
@@ -73,68 +85,68 @@ const MockActionBar: React.FC<ActionAndStatusBarProps> = ({
     );
 };
 
-describe("Parcels - Action Bar", () => {
-    const mockData: ParcelsTableRow[] = [
-        {
-            clientId: "primaryKey1",
-            addressPostcode: "AB1 2CD",
-            phoneNumber: "0987 654321",
-            deliveryCollection: {
-                collectionCentreName: "Centre 1",
-                collectionCentreAcronym: "C1",
-                congestionChargeApplies: false,
-            },
-            collectionDatetime: new Date(),
-            familyCategory: "Single",
-            fullName: "John Smith",
-            lastStatus: {
-                name: "Delivered",
-                eventData: "Some information",
-                timestamp: new Date(),
-                workflowOrder: 1,
-            },
-            packingDate: new Date(),
-            packingSlot: "AM",
-            parcelId: "123456789",
-            iconsColumn: {
-                requiresFollowUpPhoneCall: false,
-                flaggedForAttention: false,
-            },
-            voucherNumber: "123456789",
-            createdAt: new Date("2023-12-31T12:00:00+00:00"),
-            clientIsActive: true,
+const mockData: ParcelsTableRow[] = [
+    {
+        clientId: "primaryKey1",
+        addressPostcode: "AB1 2CD",
+        phoneNumber: "0987 654321",
+        deliveryCollection: {
+            collectionCentreName: "Centre 1",
+            collectionCentreAcronym: "C1",
+            congestionChargeApplies: false,
         },
-        {
-            clientId: "primaryKey2",
-            addressPostcode: "AB1 aaaa2CD",
-            phoneNumber: "+1 234 567",
-            deliveryCollection: {
-                collectionCentreName: "Centraaaae 1",
-                collectionCentreAcronym: "C2",
-                congestionChargeApplies: true,
-            },
-            collectionDatetime: new Date(),
-            familyCategory: "Family of 4",
-            fullName: "John Smaaaaith",
-            lastStatus: {
-                name: "Called and Confirmed",
-                eventData: null,
-                timestamp: new Date(),
-                workflowOrder: 2,
-            },
-            packingDate: new Date(),
-            packingSlot: "PM",
-            parcelId: "123456aaaa789",
-            iconsColumn: {
-                requiresFollowUpPhoneCall: false,
-                flaggedForAttention: false,
-            },
-            voucherNumber: "123456aaaa789",
-            createdAt: new Date("2023-12-31T12:00:00+00:00"),
-            clientIsActive: true,
+        collectionDatetime: new Date(),
+        familyCategory: "Single",
+        fullName: "John Smith",
+        lastStatus: {
+            name: "Delivered",
+            eventData: "Some information",
+            timestamp: new Date(),
+            workflowOrder: 1,
         },
-    ];
+        packingDate: new Date(),
+        packingSlot: "AM",
+        parcelId: "123456789",
+        iconsColumn: {
+            requiresFollowUpPhoneCall: false,
+            flaggedForAttention: false,
+        },
+        voucherNumber: "123456789",
+        createdAt: new Date("2023-12-31T12:00:00+00:00"),
+        clientIsActive: true,
+    },
+    {
+        clientId: "primaryKey2",
+        addressPostcode: "AB1 aaaa2CD",
+        phoneNumber: "+1 234 567",
+        deliveryCollection: {
+            collectionCentreName: "Centraaaae 1",
+            collectionCentreAcronym: "C2",
+            congestionChargeApplies: true,
+        },
+        collectionDatetime: new Date(),
+        familyCategory: "Family of 4",
+        fullName: "John Smaaaaith",
+        lastStatus: {
+            name: "Called and Confirmed",
+            eventData: null,
+            timestamp: new Date(),
+            workflowOrder: 2,
+        },
+        packingDate: new Date(),
+        packingSlot: "PM",
+        parcelId: "123456aaaa789",
+        iconsColumn: {
+            requiresFollowUpPhoneCall: false,
+            flaggedForAttention: false,
+        },
+        voucherNumber: "123456aaaa789",
+        createdAt: new Date("2023-12-31T12:00:00+00:00"),
+        clientIsActive: true,
+    },
+];
 
+describe("Parcels - Action Bar", () => {
     describe("Statuses", () => {
         let parcelIds: string[] = ["123456789", "123456aaaa789"];
         const onDeleteParcels = async (): Promise<SaveParcelStatusResult> => {
@@ -153,19 +165,26 @@ describe("Parcels - Action Bar", () => {
             );
         });
 
-        it("renders without crashing", () => {
-            expect(screen.getAllByText("Statuses")[0]).toBeInTheDocument();
+        it('renders without crashing', () => {
+            expect(screen.getAllByText('Statuses')[0]).toBeInTheDocument();
         });
+                
 
         it("should open the status menu when the status button is clicked", async () => {
-            const statusButton = screen.getByTestId("#status-button");
+            const statusButton = screen.getByTestId('#status-button');
             fireEvent.click(statusButton);
 
             await waitFor(() => {
-                expect(screen.getByTestId("#status-menu")).toBeInTheDocument();
+            expect(screen.getByTestId('#status-menu')).toBeInTheDocument();
             });
-
-            expect(screen.getByText("Parcel Denied")).toBeInTheDocument();
         });
+
+        // it("should open the modal when an item is selected", () => {
+            
+        //             });
+    
+        
     });
+
 });
+
