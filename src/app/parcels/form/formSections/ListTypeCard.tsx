637c1ee20cb8f964bdef7131d65bb0f8dbad996b
Rebecca Jesson <Rebecca.Jesson@softwire.com>
2024-07-25 11:48:57 +0100

    VFB-278 extend parcel form list type descriptive text to include client default list

--- src/app/parcels/form/formSections/ListTypeCard.tsx
+++ src/app/parcels/form/formSections/ListTypeCard.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { errorText, valueOnChangeDropdownList } from "@/components/Form/formFunctions";
 import GenericFormCard from "@/components/Form/GenericFormCard";
 import { ErrorText } from "@/components/Form/formStyling";
 import DropdownListInput from "@/components/DataInput/DropdownListInput";
 import { ParcelCardProps } from "../ParcelForm";
 import { ListTypeLabelsAndValues } from "@/common/fetch";
+import { ListName } from "@/app/lists/ListDataview";
 
 interface ListTypeCardProps extends ParcelCardProps {
     listTypeLabelsAndValues: ListTypeLabelsAndValues;
@@ -17,11 +18,24 @@ const ListTypeCard: React.FC<ListTypeCardProps> = ({
     fields,
     listTypeLabelsAndValues,
 }) => {
+    const [clientDefaultList, setClientDefaultList] = useState<ListName | null>(null);
+
+    useEffect(() => {
+        const defaultArray = listTypeLabelsAndValues.find(([label, _]) => {
+            return label.includes("default");
+        }) as [string, ListName];
+        setClientDefaultList(defaultArray ? defaultArray[1] : null);
+    }, [listTypeLabelsAndValues]);
+
     return (
         <GenericFormCard
             title="List Type"
             required={true}
-            text="Which list should be used to pack this parcel?"
+            text={
+                clientDefaultList
+                    ? `Whist list should be used to pack this parcel? The default list type for this client is ${clientDefaultList}.`
+                    : "Which list should be used to pack this parcel?"
+            }
         >
             <DropdownListInput
                 key={fields.listType}
