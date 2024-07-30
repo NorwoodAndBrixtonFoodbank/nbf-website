f64d6b902d4c8fbf523ce50675950026a9a7d321
Ebinezer Rajaram <ebinezer.rajaram@softwire.com>
2024-07-30 11:04:42 +0100

    VFB-292 Refactor config files

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "jest";
+import tsconfig from "./tsconfig.json";
+
+tsconfig.compilerOptions.jsx = "react";
+tsconfig.compilerOptions.target = "esnext";
+
+const config: Config = {
+    moduleNameMapper: {
+        "@/(.*)$": "<rootDir>/src/$1",
+        "^jose": "jose",
+    },
+    moduleFileExtensions: ["js", "jsx", "tsx", "ts"],
+    testEnvironment: "jsdom",
+    setupFiles: ["./jest.setup.ts"],
+    transform: {
+        "^.+\\.(ts|tsx)?$": [
+            "ts-jest",
+            {
+                tsconfig: tsconfig.compilerOptions,
+            },
+        ],
+    },
+    modulePaths: ["<rootDir>/src", "<rootDir>/node_modules"],
+};
+
+export default config;
