2721fb9b762e31c4b3612dbbc66397636029e1ce
Lincoln Stone <lincoln.stone@softwire.com>
2024-07-30 15:36:03 +0100

    #394: Date range filter no longer restricts the dates that can be chosen in the picker

--- src/components/DateInputs/DateRangeInputs.tsx
+++ src/components/DateInputs/DateRangeInputs.tsx
@@ -2,7 +2,7 @@
 
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from "dayjs";
-import React from "react";
+import React, { useState } from "react";
 
 const reasonableMinDate = dayjs("2020-01-01");
 
@@ -17,33 +17,60 @@ interface Props {
 }
 
 const DateRangeInputs: React.FC<Props> = (props) => {
+    const [localFromValue, setLocalFromValue] = useState<Dayjs | null>(props.range.from);
+    const [localToValue, setLocalToValue] = useState<Dayjs | null>(props.range.to);
+    const [hasErrorState, setHasErrorState] = useState<boolean>(false);
+
     const isRangeValid = (from: Dayjs, to: Dayjs): boolean => {
         return from >= reasonableMinDate && to >= reasonableMinDate && from <= to;
     };
 
     const setRangeIfPossible = (from: Dayjs | null, to: Dayjs | null): void => {
         if (from && to && isRangeValid(from, to)) {
+            setHasErrorState(false);
+
             props.setRange({
                 from: from,
                 to: to,
             });
+        } else {
+            setHasErrorState(true);
         }
+    };
+
+    const setFromValue = (fromValue: Dayjs | null): void => {
+        setLocalFromValue(fromValue);
+        setRangeIfPossible(fromValue, localToValue);
+    };
+
+    const setToValue = (toValue: Dayjs | null): void => {
+        setLocalToValue(toValue);
+        setRangeIfPossible(localFromValue, toValue);
     };
 
     return (
         <>
             <DatePicker
-                onChange={(value) => setRangeIfPossible(value, props.range.to)}
+                onChange={(value) => setFromValue(value)}
                 minDate={reasonableMinDate}
-                maxDate={props.range.to}
                 value={props.range.from}
                 label="From"
+                slotProps={{
+                    textField: {
+                        error: hasErrorState,
+                    },
+                }}
             />
             <DatePicker
-                onChange={(value) => setRangeIfPossible(props.range.from, value)}
-                minDate={props.range.from || reasonableMinDate}
+                onChange={(value) => setToValue(value)}
+                minDate={reasonableMinDate}
                 value={props.range.to}
                 label="To"
+                slotProps={{
+                    textField: {
+                        error: hasErrorState,
+                    },
+                }}
             />
         </>
     );
