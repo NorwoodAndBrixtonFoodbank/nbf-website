e792c0d081a46999aa42609b40cbf16205dc6cb9
Ebinezer Rajaram <ebinezer.rajaram@softwire.com>
2024-07-31 12:12:57 +0100

    VFB-312 Create BatchAdd.tsx

---
+++ src/app/parcels/batch/BatchAdd.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "@mui/icons-material";
+
+const BatchAdd : React.FC = () => {
+    return (
+        <>
+            <Title>Batch parcel creation - work in progress, do not use</Title>
+        </>
+    )
+}
+
+export default BatchAdd;
