89ad3b790538dcdc29ca731cf445187de571384f
Ebinezer Rajaram <ebinezer.rajaram@softwire.com>
2024-08-05 14:25:14 +0100

    VFB-312 Update test to cover display of data
    VFB-312 Remove Jest component test

--- src/app/parcels/batch/BatchParcelDataGrid.cy.tsx
+++ src/app/parcels/batch/BatchParcelDataGrid.cy.tsx
@@ -1,18 +1,37 @@
 import BatchParcelDataGrid, {
+    BatchGridDisplayRow,
     batchGridDisplayColumns,
 } from "@/app/parcels/batch/BatchParcelDataGrid";
 import mockData from "./mockData";
 describe("BatchParcelDataGrid", () => {
-    it("should display the expected column headers", () => {
-        const headerNames = batchGridDisplayColumns.map((column) => column.headerName) as string[];
-        const columnWidths = batchGridDisplayColumns.map((column) => column.width) as number[];
-        const totalWidth = columnWidths.reduce((acc, width) => acc + width, 0);
+    const fieldNames: string[] = batchGridDisplayColumns.map((column) => column.field);
+    const columnWidths = batchGridDisplayColumns.map((column) => column.width) as number[];
+    const totalWidth = columnWidths.reduce((acc, width) => acc + width, 0);
+
+    beforeEach(() => {
         cy.viewport(totalWidth + 50, 300);
         cy.mount(<BatchParcelDataGrid rows={mockData} />);
+    });
 
+    it("should display the expected column headers", () => {
+        const headerNames = batchGridDisplayColumns.map((column) => column.headerName) as string[];
         cy.get(".MuiDataGrid-columnHeaderTitle").each((header, index) => {
             const headerText = header.text().trim();
             expect(headerText).to.equal(headerNames[index]);
         });
+    });
+
+    it("should display the expected data rows", () => {
+        cy.get(".MuiDataGrid-virtualScrollerRenderZone")
+            .children()
+            .each(($row: JQuery<HTMLElement>, rowIndex: number) => {
+                cy.wrap($row)
+                    .children()
+                    .each(($item: JQuery<HTMLElement>, fieldIndex: number) => {
+                        const fieldName = fieldNames[fieldIndex] as keyof BatchGridDisplayRow;
+                        mockData[rowIndex][fieldName] &&
+                            cy.wrap($item).contains(mockData[rowIndex][fieldName]);
+                    });
+            });
     });
 });
