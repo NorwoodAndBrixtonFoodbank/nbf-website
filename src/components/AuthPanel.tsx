ff0999144cb7cc22381f9301f89f73d7811dc078
Stuart Knowles <Stuart.Knowles@softwire.com>
2024-08-20 13:47:42 +0100

    VFB-328 fix theme calls on auth panel

--- src/components/AuthPanel.tsx
+++ src/components/AuthPanel.tsx
@@ -102,8 +102,6 @@ const AuthPanel: React.FC<AuthPanelProps> = ({
     errorMessage,
     successMessage,
 }) => {
-    const theme = useTheme();
-
     return (
         <MiddleDiv elevation={5} id="login-panel">
             <Title>{title}</Title>
@@ -136,14 +134,14 @@ const AuthPanel: React.FC<AuthPanelProps> = ({
 
                 {authLinks &&
                     authLinks.map((authLink) => (
-                        <AuthLinkElement key={authLink.label} href={authLink.href} theme={theme}>
+                        <AuthLinkElement key={authLink.label} href={authLink.href}>
                             {authLink.label}
                         </AuthLinkElement>
                     ))}
 
-                {errorMessage && <ErrorMessage theme={theme}>{errorMessage}</ErrorMessage>}
+                {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
 
-                {successMessage && <SuccessMessage theme={theme}>{successMessage}</SuccessMessage>}
+                {successMessage && <SuccessMessage>{successMessage}</SuccessMessage>}
             </AuthInputSection>
         </MiddleDiv>
     );
