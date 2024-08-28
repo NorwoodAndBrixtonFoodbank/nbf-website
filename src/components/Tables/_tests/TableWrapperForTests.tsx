cd8acda3dadd3e67f740b5a40a368bd838d8135c
Jessica-Al <jessca.allen@softwire.com>
2024-08-28 11:03:15 +0100

    VFB-331 new table wrapper component for use in tests

---
+++ src/components/Tables/_tests/TableWrapperForTests.tsx
@@ -0,0 +1,243 @@
+import React, { useEffect, useState } from "react";
+import {
+    fullNameTextFilterTest,
+    MockTableProps,
+    TestData,
+    typeButtonFilterTest,
+} from "./TestingDataAndFuntions";
+import {
+    CheckboxConfig,
+    ClientPaginatedTable,
+    DefaultSortConfig,
+    EditableConfig,
+    FilterConfig,
+    OnRowClickFunction,
+    PaginationConfig,
+    SortConfig,
+    SortOptions,
+    SortState,
+} from "../Table";
+import { ClientSideSortMethod } from "../sortMethods";
+import { SortOrder } from "react-data-table-component";
+import { ClientSideFilter, DistributeClientFilter } from "../Filters";
+
+export const TableWrapperForTest: React.FC<MockTableProps<TestData>> = ({
+    mockData,
+    mockHeaders,
+    testableContent: {
+        isCheckboxIncluded = false,
+        filterFlags = { isPrimaryFilterIncluded: false, isAdditionalFilterIncluded: false },
+        isPaginationIncluded = false,
+        sortingFlags = { isSortingOptionsIncluded: false, isDefaultSortIncluded: false },
+        editableFlags = { isEditIncluded: false, isDeleteIncluded: false, isSwapIncluded: false },
+        isHeaderTogglesIncluded = false,
+        isColumnDisplayFunctionsIncluded = false,
+        isRowClickIncluded = false,
+    },
+}) => {
+    //to show text on page when action occurs
+    const [shownText, setShownText] = useState<string>("");
+
+    const [testDataPortion, setTestDataPortion] = useState<TestData[]>(mockData);
+
+    //Checkbox setup
+    const [checkedRowIds, setCheckedRowIds] = useState<string[]>([]);
+    const [isAllCheckBoxSelected, setAllCheckBoxSelected] = useState(false);
+
+    useEffect(() => {
+        const allChecked = checkedRowIds.length === testDataPortion.length;
+        if (allChecked !== isAllCheckBoxSelected) {
+            setAllCheckBoxSelected(allChecked);
+        }
+    }, [testDataPortion.length, checkedRowIds, isAllCheckBoxSelected]);
+
+    const checkboxConfig: CheckboxConfig<TestData> = isCheckboxIncluded
+        ? {
+              displayed: true,
+              selectedRowIds: checkedRowIds,
+              isAllCheckboxChecked: isAllCheckBoxSelected,
+              onCheckboxClicked: (data: TestData) => {
+                  setCheckedRowIds((checkedIds) => {
+                      if (checkedIds.includes(data.id)) {
+                          return checkedIds.filter((checkedId) => checkedId !== data.id);
+                      }
+                      return checkedIds.concat([data.id]);
+                  });
+              },
+              onAllCheckboxClicked: () => {
+                  if (isAllCheckBoxSelected === true) {
+                      setCheckedRowIds([]);
+                      setAllCheckBoxSelected(false);
+                  } else {
+                      setCheckedRowIds(mockData.map((row) => row.id));
+                      setAllCheckBoxSelected(true);
+                  }
+              },
+              isRowChecked: (row: TestData) => checkedRowIds.includes(row.id),
+          }
+        : { displayed: false };
+
+    //Filters setup
+    const [primaryFilters, setPrimaryFilters] = useState<ClientSideFilter<TestData, string>[]>([
+        fullNameTextFilterTest,
+    ]);
+    const [additionalFilters, setAdditionalFilters] = useState<
+        ClientSideFilter<TestData, string>[]
+    >([typeButtonFilterTest]);
+
+    const filterConfig: FilterConfig<ClientSideFilter<TestData, string>> =
+        filterFlags.isPrimaryFilterIncluded && filterFlags.isAdditionalFilterIncluded
+            ? {
+                  primaryFiltersShown: true,
+                  primaryFilters: [fullNameTextFilterTest],
+                  setPrimaryFilters: setPrimaryFilters,
+                  additionalFiltersShown: true,
+                  additionalFilters: [typeButtonFilterTest],
+                  setAdditionalFilters: setAdditionalFilters,
+              }
+            : filterFlags.isPrimaryFilterIncluded
+              ? {
+                    primaryFiltersShown: true,
+                    primaryFilters: [fullNameTextFilterTest],
+                    setPrimaryFilters: setPrimaryFilters,
+                    additionalFiltersShown: false,
+                }
+              : { primaryFiltersShown: false, additionalFiltersShown: false };
+
+    //Pagination setup
+    const [perPage, setPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(1);
+    const startPoint = (currentPage - 1) * perPage;
+    const endPoint = currentPage * perPage - 1;
+
+    useEffect(() => {
+        setTestDataPortion(mockData.slice(startPoint, endPoint + 1));
+    }, [startPoint, endPoint, mockData]);
+
+    const paginationConfig: PaginationConfig = isPaginationIncluded
+        ? {
+              enablePagination: true,
+              filteredCount: mockData.length,
+              onPageChange: setCurrentPage,
+              onPerPageChange: setPerPage,
+          }
+        : { enablePagination: false };
+
+    //Sorting setup
+    const [sortState, setSortState] = useState<SortState<TestData, ClientSideSortMethod>>({
+        sortEnabled: false,
+    });
+
+    const sortableColumns: SortOptions<TestData, ClientSideSortMethod>[] = [];
+    for (const key of mockHeaders
+        .map(([key, _]) => {
+            return key;
+        })
+        .filter((key, index) => key !== "id" && index % 2 == 0)) {
+        sortableColumns.push({
+            key: key,
+            sortMethod: (sortDirection: SortOrder) => {
+                const ascendingData = [...testDataPortion].sort((rowA, rowB) =>
+                    rowA.full_name > rowB.full_name ? 1 : rowB.full_name > rowA.full_name ? -1 : 0
+                );
+                if (sortDirection === "asc") {
+                    setTestDataPortion(ascendingData);
+                } else {
+                    setTestDataPortion([...ascendingData].reverse());
+                }
+            },
+        });
+    }
+
+    const sortConfig: SortConfig<TestData, ClientSideSortMethod> =
+        sortingFlags.isSortingOptionsIncluded
+            ? {
+                  sortPossible: true,
+                  sortableColumns: sortableColumns,
+                  setSortState: setSortState,
+              }
+            : { sortPossible: false };
+
+    const defaultSortConfig: DefaultSortConfig | undefined = sortingFlags.isDefaultSortIncluded
+        ? {
+              defaultColumnHeaderKey: mockHeaders[0][0],
+              defaultSortDirection: "asc" as SortOrder,
+          }
+        : undefined;
+
+    useEffect(() => {
+        if (sortState.sortEnabled && sortState.column.sortMethod) {
+            sortState.column.sortMethod(sortState.sortDirection);
+        }
+    }, [sortState, testDataPortion]);
+
+    //Editable setup
+    const editableConfig: EditableConfig<TestData> = {
+        editable:
+            editableFlags.isEditIncluded ||
+            editableFlags.isDeleteIncluded ||
+            editableFlags.isSwapIncluded,
+        setDataPortion: setTestDataPortion,
+        onEdit: editableFlags.isEditIncluded
+            ? (row_num) => {
+                  setShownText("Edit clicked: " + row_num);
+              }
+            : undefined,
+        onDelete: editableFlags.isDeleteIncluded
+            ? (row_num) => {
+                  setShownText("Delete clicked: " + row_num);
+              }
+            : undefined,
+        onSwapRows: editableFlags.isSwapIncluded
+            ? async (row1, row2) => {
+                  async () => {
+                      setShownText("Swapped rows: " + row1 + " and " + row2);
+                  };
+              }
+            : undefined,
+        isDeletable: editableFlags.isDeleteIncluded ? (row) => row.id != "0" : undefined,
+    };
+
+    //Header toggles setup
+    const defaultShownHeaders: (keyof TestData)[] | undefined = isHeaderTogglesIncluded
+        ? mockHeaders
+              .map(([key, _]) => {
+                  return key;
+              })
+              .slice(0, mockHeaders.length - 1)
+        : undefined;
+    const toggleableHeaders: (keyof TestData)[] | undefined = isHeaderTogglesIncluded
+        ? mockHeaders
+              .map(([key, _]) => {
+                  return key;
+              })
+              .slice(1, mockHeaders.length)
+        : undefined;
+
+    //Create onRowClick function
+    const onRowClick: OnRowClickFunction<TestData> | undefined = isRowClickIncluded
+        ? (row) => {
+              setShownText("row clicked " + row.data[mockHeaders[0][0]]);
+          }
+        : undefined;
+
+    //Render table with mock content
+    return (
+        <>
+            <ClientPaginatedTable
+                dataPortion={testDataPortion}
+                headerKeysAndLabels={mockHeaders}
+                checkboxConfig={checkboxConfig}
+                filterConfig={filterConfig}
+                paginationConfig={paginationConfig}
+                sortConfig={sortConfig}
+                defaultSortConfig={defaultSortConfig}
+                editableConfig={editableConfig}
+                defaultShownHeaders={defaultShownHeaders}
+                toggleableHeaders={toggleableHeaders}
+                onRowClick={onRowClick}
+            />
+            <p>{shownText}</p>
+        </>
+    );
+};
---
+++ src/components/Tables/_tests/TestingDataAndFuntions.ts
@@ -0,0 +1,152 @@
+import { LIST_TYPES_ARRAY } from "@/common/fetch";
+import { buttonGroupFilter, filterRowbyButton } from "../ButtonFilter";
+import { buildClientSideTextFilter, filterRowByText } from "../TextFilter";
+import { CheckboxConfig, ClientPaginatedTable, TableHeaders } from "../Table";
+import { useEffect, useState } from "react";
+
+export interface MockTableProps<Data> {
+    mockData: Data[];
+    mockHeaders: TableHeaders<Data>;
+    testableContent: TestableContent;
+}
+
+interface TestableContent {
+    isCheckboxIncluded?: boolean;
+    filterFlags?: { isPrimaryFilterIncluded: boolean; isAdditionalFilterIncluded: boolean };
+    isPaginationIncluded?: boolean;
+    sortingFlags?: { isSortingOptionsIncluded: boolean; isDefaultSortIncluded: boolean };
+    editableFlags?: { isEditIncluded: boolean; isDeleteIncluded: boolean; isSwapIncluded: boolean };
+    isHeaderTogglesIncluded?: boolean;
+    isColumnDisplayFunctionsIncluded?: boolean;
+    isRowClickIncluded?: boolean;
+}
+
+export interface TestData {
+    full_name: string;
+    phone_number: string;
+    type: "hotel" | "regular";
+    id: string;
+}
+
+export const fakeData: TestData[] = [
+    {
+        full_name: "Tom",
+        phone_number: "123456",
+        type: "regular",
+        id: "0",
+    },
+    {
+        full_name: "Sam",
+        phone_number: "999",
+        type: "hotel",
+        id: "1",
+    },
+    {
+        full_name: "Harper Garrett",
+        phone_number: "2171786554",
+        type: "regular",
+        id: "2",
+    },
+    {
+        full_name: "Adrian Key",
+        phone_number: "3650099130",
+        type: "hotel",
+        id: "3",
+    },
+    {
+        full_name: "Harrell Wallace",
+        phone_number: "4650047935",
+        type: "regular",
+        id: "4",
+    },
+    {
+        full_name: "Oneill Curtis",
+        phone_number: "7058491995",
+        type: "regular",
+        id: "5",
+    },
+    {
+        full_name: "Herring Rutledge",
+        phone_number: "1440882899",
+        type: "regular",
+        id: "6",
+    },
+    {
+        full_name: "Eloise Rowland",
+        phone_number: "2580325390",
+        type: "regular",
+        id: "7",
+    },
+    {
+        full_name: "Cathryn Burks",
+        phone_number: "7136166489",
+        type: "regular",
+        id: "8",
+    },
+    {
+        full_name: "Paopao",
+        phone_number: "7136166469",
+        type: "regular",
+        id: "9",
+    },
+    {
+        full_name: "Forbes Doyle",
+        phone_number: "1377097191",
+        type: "regular",
+        id: "10",
+    },
+    {
+        full_name: "Agnes Rosales",
+        phone_number: "3334796379",
+        type: "regular",
+        id: "11",
+    },
+    {
+        full_name: "Jan Orr",
+        phone_number: "1526538148",
+        type: "regular",
+        id: "12",
+    },
+    {
+        full_name: "Colleen Lowery",
+        phone_number: "3980156139",
+        type: "regular",
+        id: "13",
+    },
+    {
+        full_name: "Chloe",
+        phone_number: "4567894522",
+        type: "regular",
+        id: "14",
+    },
+];
+
+export const fakeSmallerData = fakeData.slice(0, 3);
+
+export const fakeMidData = fakeData.slice(0, 7);
+
+export const fakeDataHeaders = [
+    ["full_name", "Name"],
+    ["phone_number", "Phone Number"],
+    ["type", "Type"],
+] as const;
+
+export const selectorForRowCheckboxWithAriaLabel = (ariaLabel: string): string => {
+    return "span[aria-label='" + ariaLabel + "'] > input[type='checkbox']";
+};
+
+export const fullNameTextFilterTest = buildClientSideTextFilter<TestData>({
+    key: "full_name",
+    headers: fakeDataHeaders,
+    label: "Name",
+    method: filterRowByText,
+});
+
+export const typeButtonFilterTest = buttonGroupFilter<TestData>({
+    key: "type",
+    filterLabel: "",
+    filterOptions: LIST_TYPES_ARRAY,
+    initialActiveFilter: "regular",
+    method: filterRowbyButton,
+    shouldPersistOnClear: true,
+});
