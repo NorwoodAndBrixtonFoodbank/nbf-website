eb01dd16eeb6879983635d19a3872a35b1e3b61d
Stuart Knowles <Stuart.Knowles@softwire.com>
2024-09-04 11:41:19 +0100

    Update batch-create page data

--- src/app/batch-create/page.tsx
+++ src/app/batch-create/page.tsx
@@ -1,19 +1,19 @@
 import { Metadata } from "next";
 import React from "react";
 import Title from "../../components/Title/Title";
-import BatchParcelDataGrid from "@/app/batch-create/displayComponents/BatchParcelDataGrid";
+import BatchParcelDataGrid from "@/app/batch-create/BatchParcelDataGrid";
 
 const Batch: () => Promise<React.ReactElement> = async () => {
     return (
         <main>
-            <Title>Batch parcel creation - work in progress, do not use</Title>
+            <Title>Batch client and parcel creation</Title>
             <BatchParcelDataGrid />
         </main>
     );
 };
 
 export const metadata: Metadata = {
-    title: "Batch",
+    title: "Batch Creation",
 };
 
 export default Batch;
