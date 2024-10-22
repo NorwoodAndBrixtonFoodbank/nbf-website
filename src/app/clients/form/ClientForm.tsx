92b562d4c091dee1cc2e1ef08b464644ed04aa5c
Lincoln Stone <lincoln.stone@softwire.com>
2024-10-22 22:53:03 +0100

    #401: Lint fix

--- src/app/clients/form/ClientForm.tsx
+++ src/app/clients/form/ClientForm.tsx
@@ -38,7 +38,6 @@ import { submitAddClientForm, submitEditClientForm } from "@/app/clients/form/su
 import Title from "@/components/Title/Title";
 import ClientNotesCard from "@/app/clients/form/formSections/ClientNotes";
 import { getCurrentYear } from "@/common/date";
-import { youngestAdultBirthYear } from "@/app/clients/form/birthYearDropdown";
 import ListTypeCard from "@/app/clients/form/formSections/ListTypeCard";
 import { ListType } from "@/common/databaseListTypes";
 
